4f842d2839624ae561cef4c2a0dd3cd456810e71
Bryan Chen <xlchen1291@gmail.com>
2019-12-02 12:01:02 +1300

    reduce block time to 4s

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -105,7 +105,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	apis: RUNTIME_API_VERSIONS,
 };
 
-pub const MILLISECS_PER_BLOCK: u64 = 6000;
+pub const MILLISECS_PER_BLOCK: u64 = 4000;
 
 pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
 
