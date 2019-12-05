9a4dc5e9d8453cb3548db57eb025681826142c07
Bryan Chen <xlchen1291@gmail.com>
2019-12-05 13:05:03 +1300

    add publish docker hub script

---
+++ scripts/docker-hub-publish.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+VERSION=$1
+
+if [[ -z "$1" ]] ; then
+    echo "Usage: ./scripts/docker-hub-publish.sh VERSION"
+    exit 1
+fi
+
+docker build . -t acala/acala-node:$1 -t acala/acala-node:latest
+docker push acala/acala-node:$1
+docker push acala/acala-node:latest
