fd5724a3a5753d3de099c9f90f6197c41d7c3ebe
Bryan Chen <xlchen1291@gmail.com>
2019-12-06 10:25:41 +1300

    regenerate genesis

--- scripts/update-alpha.sh
+++ scripts/update-alpha.sh
@@ -2,5 +2,6 @@
 
 set -e
 
+cargo clean
 WASM_BUILD_TYPE=release cargo run -- build-spec --chain alpha-latest > ./resources/alpha.json
 WASM_BUILD_TYPE=release cargo run -- build-spec --chain ./resources/alpha.json --raw > ./resources/alpha-dist.json
