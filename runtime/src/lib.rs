1ead107e188cdcd4c24e51d6153150c4aff439c6
Xiliang Chen <xlchen1291@gmail.com>
2020-01-13 11:22:03 +1300

    bump runtime spec version (#52)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -100,7 +100,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("acala"),
 	impl_name: create_runtime_str!("acala"),
 	authoring_version: 1,
-	spec_version: 1,
+	spec_version: 2,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 };
