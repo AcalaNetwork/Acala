8b72fd4bad7e7b0aceb766ac6323f5249c28f4a8
Bryan Chen <xlchen1291@gmail.com>
2020-02-13 12:42:03 +1300

    runtime 27

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -54,7 +54,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("acala"),
 	impl_name: create_runtime_str!("acala"),
 	authoring_version: 1,
-	spec_version: 26,
+	spec_version: 27,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 };
