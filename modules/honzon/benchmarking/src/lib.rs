b9b1eaf5fee4b8201be42b49c63498140c8670ca
wangjj9219 <183318287@qq.com>
2020-05-06 10:30:33 +0800

    modify

--- modules/honzon/benchmarking/src/lib.rs
+++ modules/honzon/benchmarking/src/lib.rs
@@ -8,18 +8,18 @@ mod mock;
 use sp_std::prelude::*;
 use sp_std::vec;
 
-use frame_system::RawOrigin;
-use frame_benchmarking::{account, benchmarks};
-use frame_support::traits::Get;
-use sp_runtime::traits::UniqueSaturatedInto;
-use sp_std::prelude::*;
+// use frame_system::RawOrigin;
+// use frame_benchmarking::{account, benchmarks};
+// use frame_support::traits::Get;
+// use sp_runtime::traits::UniqueSaturatedInto;
+// use sp_std::prelude::*;
 
-use module_honzon::*;
-use module_honzon::Module as Honzon;
-use module_cdp_engine::Module as CdpEngine;
-use module_loans::Module as Loans;
-use orml_oracle::Module as Oracle;
+// use module_honzon::*;
+// use module_honzon::Module as Honzon;
+// use module_cdp_engine::Module as CdpEngine;
+// use module_loans::Module as Loans;
+// use orml_oracle::Module as Oracle;
 
-pub struct Module<T: Trait>(pallet_session::Module<T>);
+// pub struct Module<T: Trait>(pallet_session::Module<T>);
 
-pub trait Trait: module_honzon::Trait + orml_oracle::Trait {}
+// pub trait Trait: module_honzon::Trait + orml_oracle::Trait {}
