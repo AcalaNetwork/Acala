60098b5e7934ef373ebc19b0165a10cddd0244da
Shaopeng Wang <spxwang@gmail.com>
2020-05-20 05:12:46 +1200

    Orml tokens benchmarking. (#233)
    Co-authored-by: wangjj9219 <183318287@qq.com>

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -55,6 +55,7 @@ pub use sp_runtime::{Perbill, Percent, Permill};
 pub use constants::{currency::*, time::*};
 pub use types::*;
 
+mod benchmarking;
 mod constants;
 mod types;
 
@@ -1113,6 +1114,29 @@ impl_runtime_apis! {
 
 		fn get_liquid_staking_exchange_rate() -> ExchangeRate {
 			StakingPool::liquid_exchange_rate()
+		}
+	}
+
+	#[cfg(feature = "runtime-benchmarks")]
+	impl frame_benchmarking::Benchmark<Block> for Runtime {
+		fn dispatch_benchmark(
+			pallet: Vec<u8>,
+			benchmark: Vec<u8>,
+			lowest_range_values: Vec<u32>,
+			highest_range_values: Vec<u32>,
+			steps: Vec<u32>,
+			repeat: u32,
+		) -> Result<Vec<frame_benchmarking::BenchmarkBatch>, sp_runtime::RuntimeString> {
+			use frame_benchmarking::{Benchmarking, BenchmarkBatch};
+			use orml_benchmarking::add_benchmark;
+
+			let mut batches = Vec::<BenchmarkBatch>::new();
+			let params = (&pallet, &benchmark, &lowest_range_values, &highest_range_values, &steps, repeat);
+
+			add_benchmark!(params, batches, b"tokens", benchmarking::tokens);
+
+			if batches.is_empty() { return Err("Benchmark not found for this module.".into()) }
+			Ok(batches)
 		}
 	}
 }
