cd9c1d06d5872c08bd9709e83924b459155e83d5
Bryan Chen <xlchen1291@gmail.com>
2020-05-21 12:13:49 +1200

    update orml

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -32,7 +32,6 @@ use sp_version::RuntimeVersion;
 
 use frame_system::{self as system};
 use orml_currencies::{BasicCurrencyAdapter, Currency};
-use orml_oracle::OperatorProvider;
 use pallet_grandpa::fg_primitives;
 use pallet_grandpa::{AuthorityId as GrandpaId, AuthorityList as GrandpaAuthorityList};
 use pallet_session::historical as pallet_session_historical;
@@ -320,20 +319,6 @@ impl pallet_membership::Trait<TechnicalCouncilMembershipInstance> for Runtime {
 	type MembershipChanged = TechnicalCouncil;
 }
 
-parameter_types! {
-	pub const OperatorMotionDuration: BlockNumber = 0;
-	pub const OperatorMaxProposals: u32 = 100;
-}
-
-type OperatorCollectiveInstance = pallet_collective::Instance5;
-impl pallet_collective::Trait<OperatorCollectiveInstance> for Runtime {
-	type Origin = Origin;
-	type Proposal = Call;
-	type Event = Event;
-	type MotionDuration = OperatorMotionDuration;
-	type MaxProposals = OperatorMaxProposals;
-}
-
 type OperatorMembershipInstance = pallet_membership::Instance5;
 impl pallet_membership::Trait<OperatorMembershipInstance> for Runtime {
 	type Event = Event;
@@ -342,8 +327,8 @@ impl pallet_membership::Trait<OperatorMembershipInstance> for Runtime {
 	type SwapOrigin = pallet_collective::EnsureProportionMoreThan<_1, _3, AccountId, GeneralCouncilInstance>;
 	type ResetOrigin = pallet_collective::EnsureProportionMoreThan<_1, _3, AccountId, GeneralCouncilInstance>;
 	type PrimeOrigin = pallet_collective::EnsureProportionMoreThan<_1, _3, AccountId, GeneralCouncilInstance>;
-	type MembershipInitialized = OperatorCollective;
-	type MembershipChanged = OperatorCollective;
+	type MembershipInitialized = Oracle;
+	type MembershipChanged = Oracle;
 }
 
 parameter_types! {
@@ -523,32 +508,22 @@ impl orml_auction::Trait for Runtime {
 	type Handler = AuctionManager;
 }
 
-pub struct OperatorCollectiveProvider;
-impl OperatorProvider<AccountId> for OperatorCollectiveProvider {
-	fn can_feed_data(who: &AccountId) -> bool {
-		OperatorCollective::is_member(who)
-	}
-
-	fn operators() -> Vec<AccountId> {
-		OperatorCollective::members()
-	}
-}
-
 parameter_types! {
 	pub const MinimumCount: u32 = 1;
 	pub const ExpiresIn: Moment = 1000 * 60 * 30; // 30 mins
+	pub const OracleUnsignedPriority: TransactionPriority = TransactionPriority::max_value() - 1;
 }
 
 impl orml_oracle::Trait for Runtime {
 	type Event = Event;
 	type Call = Call;
 	type OnNewData = ();
-	type OnRedundantCall = ();
-	type OperatorProvider = OperatorCollectiveProvider;
 	type CombineData = orml_oracle::DefaultCombineData<Runtime, MinimumCount, ExpiresIn>;
 	type Time = Timestamp;
 	type OracleKey = CurrencyId;
 	type OracleValue = Price;
+	type UnsignedPriority = OracleUnsignedPriority;
+	type AuthorityId = orml_oracle::AuthorityId;
 }
 
 pub type TimeStampedPrice = orml_oracle::TimestampedValueOf<Runtime>;
@@ -608,6 +583,7 @@ impl orml_schedule_update::Trait for Runtime {
 	type Event = Event;
 	type Call = Call;
 	type MaxScheduleDispatchWeight = MaxScheduleDispatchWeight;
+	type DispatchOrigin = system::EnsureRoot<AccountId>;
 }
 
 parameter_types! {
@@ -882,7 +858,6 @@ construct_runtime!(
 		HomaCouncilMembership: pallet_membership::<Instance3>::{Module, Call, Storage, Event<T>, Config<T>},
 		TechnicalCouncil: pallet_collective::<Instance4>::{Module, Call, Storage, Origin<T>, Event<T>, Config<T>},
 		TechnicalCouncilMembership: pallet_membership::<Instance4>::{Module, Call, Storage, Event<T>, Config<T>},
-		OperatorCollective: pallet_collective::<Instance5>::{Module, Call, Storage, Origin<T>, Event<T>, Config<T>},
 		OperatorMembership: pallet_membership::<Instance5>::{Module, Call, Storage, Event<T>, Config<T>},
 
 		// acala modules
