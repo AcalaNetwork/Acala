da90695174b00128dcfb150d632160aa4c28e864
Jipyeongseon Geunyang Samgagbeob <prin-r@users.noreply.github.com>
2020-08-24 11:32:11 +0700

    Update runtime/dev/src/lib.rs
    Co-authored-by: Shaopeng Wang <spxwang@gmail.com>

--- runtime/dev/src/lib.rs
+++ runtime/dev/src/lib.rs
@@ -699,7 +699,7 @@ impl orml_oracle::Trait<BandDataProvider> for Runtime {
 
 pub type TimeStampedPrice = TimestampedValue<Price, Moment>;
 
-create_median_value_data_provider!(AggregatedDataProviderImpl, AcalaOracle, BandOracle);
+create_median_value_data_provider!(AggregatedDataProvider, AcalaOracle, BandOracle);
 
 struct DataProviders;
 impl MultiDataProvider<DataProviderId, CurrencyId, Price> for MultiDataProviderImpl {
