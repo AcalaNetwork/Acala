2793ba7ac518b4d2ee6231f0734a9809caabd31e
Bryan Chen <xlchen1291@gmail.com>
2020-10-16 14:59:57 +1300

    bump version

--- runtime/acala/src/lib.rs
+++ runtime/acala/src/lib.rs
@@ -87,7 +87,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("acala"),
 	impl_name: create_runtime_str!("acala"),
 	authoring_version: 1,
-	spec_version: 601,
+	spec_version: 100,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
