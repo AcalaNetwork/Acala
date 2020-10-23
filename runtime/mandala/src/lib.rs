512f2e27c12404b9f87dc6382c2748605a161617
qiuhao <qiuhao951@gmail.com>
2020-10-23 14:02:12 +0800

    bump version

--- runtime/mandala/src/lib.rs
+++ runtime/mandala/src/lib.rs
@@ -88,7 +88,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("mandala"),
 	impl_name: create_runtime_str!("mandala"),
 	authoring_version: 1,
-	spec_version: 603,
+	spec_version: 604,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
