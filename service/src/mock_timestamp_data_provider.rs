79cd788576abcdc07fcdc96173a1e34b4e476ccd
Ermal Kaleci <ermalkaleci@gmail.com>
2020-11-17 23:33:11 +0100

    fix

--- service/src/mock_timestamp_data_provider.rs
+++ service/src/mock_timestamp_data_provider.rs
@@ -1,13 +1,6 @@
 use sp_inherents::{InherentData, InherentIdentifier, ProvideInherentData};
 use std::cell::RefCell;
 
-#[cfg(feature = "with-acala-runtime")]
-use acala_runtime::SLOT_DURATION;
-#[cfg(feature = "with-karura-runtime")]
-use karura_runtime::SLOT_DURATION;
-#[cfg(feature = "with-mandala-runtime")]
-use mandala_runtime::SLOT_DURATION;
-
 /// Provide a mock duration starting at 0 in millisecond for timestamp inherent.
 /// Each call will increment timestamp by slot_duration making block importer
 /// think time has passed.
@@ -24,7 +17,7 @@ impl ProvideInherentData for MockTimestampInherentDataProvider {
 
 	fn provide_inherent_data(&self, inherent_data: &mut InherentData) -> Result<(), sp_inherents::Error> {
 		TIMESTAMP.with(|x| {
-			*x.borrow_mut() += SLOT_DURATION;
+			*x.borrow_mut() += 4_000;
 			inherent_data.put_data(INHERENT_IDENTIFIER, &*x.borrow())
 		})
 	}
