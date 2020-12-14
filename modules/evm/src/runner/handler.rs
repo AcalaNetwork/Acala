8810d84d329e24a9234ea8c3542f1b5890b757e5
Bryan Chen <xlchen1291@gmail.com>
2020-12-14 14:27:47 +1300

    correctly handle gas cost for precompile

--- modules/evm/src/runner/handler.rs
+++ modules/evm/src/runner/handler.rs
@@ -451,11 +451,12 @@ impl<'vicinity, 'config, T: Config> HandlerT for Handler<'vicinity, 'config, T> 
 	) -> Capture<(ExitReason, Vec<u8>), Self::CallInterrupt> {
 		debug::debug!(
 			target: "evm",
-			"handler: call: source {:?} code_address {:?} input: {:?} target_gas: {:?}",
+			"handler: call: source {:?} code_address {:?} input: {:?} target_gas {:?} gas_left {:?}",
 			context.caller,
 			code_address,
 			input,
 			target_gas,
+			self.gas_left()
 		);
 
 		create_try!(|e: ExitError| (e.into(), Vec::new()));
@@ -476,8 +477,6 @@ impl<'vicinity, 'config, T: Config> HandlerT for Handler<'vicinity, 'config, T> 
 				target_gas = target_gas.saturating_add(self.config.call_stipend);
 			}
 		}
-
-		try_or_fail!(self.gasometer.record_cost(target_gas));
 
 		let code = self.code(code_address);
 
@@ -510,12 +509,14 @@ impl<'vicinity, 'config, T: Config> HandlerT for Handler<'vicinity, 'config, T> 
 				};
 			}
 
+			try_or_rollback!(self.gasometer.record_cost(target_gas));
+
 			let (reason, out) = substate.execute(context.caller, context.address, context.apparent_value, code, input);
 
 			debug::debug!(
 				target: "evm",
-				"handler: call-result: reason {:?} out {:?}",
-				reason, out
+				"handler: call-result: reason {:?} out {:?} gas_left {:?}",
+				reason, out, substate.gas_left()
 			);
 
 			match reason {
