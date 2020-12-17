e9d21ffae2d39f13ff28fbec5e9176970536fed8
wangjj9219 <183318287@qq.com>
2020-12-17 14:57:29 +0800

    update weights info

--- modules/homa/src/default_weight.rs
+++ modules/homa/src/default_weight.rs
@@ -7,26 +7,26 @@ use frame_support::weights::{constants::RocksDbWeight as DbWeight, Weight};
 
 impl crate::WeightInfo for () {
 	fn mint() -> Weight {
-		(95_000_000 as Weight)
-			.saturating_add(DbWeight::get().reads(9 as Weight))
+		(71_000_000 as Weight)
+			.saturating_add(DbWeight::get().reads(6 as Weight))
 			.saturating_add(DbWeight::get().writes(6 as Weight))
 	}
 	fn redeem(strategy: &RedeemStrategy) -> Weight {
 		match strategy {
-			RedeemStrategy::Immediately => (108_000_000 as Weight)
-				.saturating_add(DbWeight::get().reads(9 as Weight))
+			RedeemStrategy::Immediately => (88_000_000 as Weight)
+				.saturating_add(DbWeight::get().reads(6 as Weight))
 				.saturating_add(DbWeight::get().writes(5 as Weight)),
-			RedeemStrategy::Target(_) => (83_000_000 as Weight)
-				.saturating_add(DbWeight::get().reads(10 as Weight))
+			RedeemStrategy::Target(_) => (75_000_000 as Weight)
+				.saturating_add(DbWeight::get().reads(7 as Weight))
 				.saturating_add(DbWeight::get().writes(5 as Weight)),
-			RedeemStrategy::WaitForUnbonding => (59_000_000 as Weight)
-				.saturating_add(DbWeight::get().reads(8 as Weight))
+			RedeemStrategy::WaitForUnbonding => (47_000_000 as Weight)
+				.saturating_add(DbWeight::get().reads(4 as Weight))
 				.saturating_add(DbWeight::get().writes(4 as Weight)),
 		}
 	}
 	fn withdraw_redemption() -> Weight {
-		(65_000_000 as Weight)
+		(53_000_000 as Weight)
 			.saturating_add(DbWeight::get().reads(6 as Weight))
-			.saturating_add(DbWeight::get().writes(4 as Weight))
+			.saturating_add(DbWeight::get().writes(2 as Weight))
 	}
 }
--- runtime/acala/src/weights/homa.rs
+++ runtime/acala/src/weights/homa.rs
@@ -5,26 +5,26 @@ use sp_std::marker::PhantomData;
 pub struct WeightInfo<T>(PhantomData<T>);
 impl<T: frame_system::Config> module_homa::WeightInfo for WeightInfo<T> {
 	fn mint() -> Weight {
-		(95_000_000 as Weight)
-			.saturating_add(DbWeight::get().reads(9 as Weight))
+		(71_000_000 as Weight)
+			.saturating_add(DbWeight::get().reads(6 as Weight))
 			.saturating_add(DbWeight::get().writes(6 as Weight))
 	}
 	fn redeem(strategy: &module_homa::RedeemStrategy) -> Weight {
 		match strategy {
-			module_homa::RedeemStrategy::Immediately => (108_000_000 as Weight)
-				.saturating_add(DbWeight::get().reads(9 as Weight))
+			module_homa::RedeemStrategy::Immediately => (88_000_000 as Weight)
+				.saturating_add(DbWeight::get().reads(6 as Weight))
 				.saturating_add(DbWeight::get().writes(5 as Weight)),
-			module_homa::RedeemStrategy::Target(_) => (83_000_000 as Weight)
-				.saturating_add(DbWeight::get().reads(10 as Weight))
+			module_homa::RedeemStrategy::Target(_) => (75_000_000 as Weight)
+				.saturating_add(DbWeight::get().reads(7 as Weight))
 				.saturating_add(DbWeight::get().writes(5 as Weight)),
-			module_homa::RedeemStrategy::WaitForUnbonding => (59_000_000 as Weight)
-				.saturating_add(DbWeight::get().reads(8 as Weight))
+			module_homa::RedeemStrategy::WaitForUnbonding => (47_000_000 as Weight)
+				.saturating_add(DbWeight::get().reads(4 as Weight))
 				.saturating_add(DbWeight::get().writes(4 as Weight)),
 		}
 	}
 	fn withdraw_redemption() -> Weight {
-		(65_000_000 as Weight)
+		(53_000_000 as Weight)
 			.saturating_add(DbWeight::get().reads(6 as Weight))
-			.saturating_add(DbWeight::get().writes(4 as Weight))
+			.saturating_add(DbWeight::get().writes(2 as Weight))
 	}
 }
