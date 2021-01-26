b229e56d92ce48983aff4016ed053ade275e6537
Xiliang Chen <xlchen1291@gmail.com>
2021-01-26 17:10:45 +1300

    State rent refactor (#690)
    * wip
    
    * wip
    
    * it compiles
    
    * fix build issue
    
    * fix build error
    
    * more fixes
    
    * some more fix
    
    * more fixes
    
    * fix
    
    * fix warnings
    
    * fix
    
    * fix test
    
    * fix

--- modules/evm/src/lib.rs
+++ modules/evm/src/lib.rs
@@ -34,7 +34,7 @@ use serde::{Deserialize, Serialize};
 use sha3::{Digest, Keccak256};
 use sp_core::{H256, U256};
 use sp_runtime::{
-	traits::{Convert, DispatchInfoOf, One, PostDispatchInfoOf, Saturating, SignedExtension, UniqueSaturatedInto},
+	traits::{Convert, DispatchInfoOf, One, PostDispatchInfoOf, SignedExtension, UniqueSaturatedInto},
 	transaction_validity::TransactionValidityError,
 	Either, TransactionOutcome,
 };
@@ -140,21 +140,11 @@ pub trait Config: frame_system::Config + pallet_timestamp::Config {
 	type WeightInfo: WeightInfo;
 }
 
-/// Storage key size and storage value size.
-pub const STORAGE_SIZE: u32 = 64;
-
 #[derive(Clone, Eq, PartialEq, RuntimeDebug, Encode, Decode)]
 pub struct ContractInfo {
-	pub storage_count: u32,
 	pub code_hash: H256,
 	pub maintainer: EvmAddress,
 	pub deployed: bool,
-}
-
-impl ContractInfo {
-	pub fn total_storage_size(&self) -> u32 {
-		self.storage_count.saturating_mul(STORAGE_SIZE)
-	}
 }
 
 #[derive(Clone, Eq, PartialEq, RuntimeDebug, Encode, Decode)]
@@ -225,7 +215,7 @@ decl_storage! {
 				);
 
 				if !account.code.is_empty() { // if code len > 0 then it's a contract
-					<Module<T>>::on_contract_initialization(address, &EvmAddress::default(), account.code.clone(), Some(account.storage.len() as u32)).expect("Genesis contract shouldn't fail");
+					<Module<T>>::on_contract_initialization(address, &EvmAddress::default(), account.code.clone()).expect("Genesis contract shouldn't fail");
 
 					#[cfg(not(feature = "with-ethereum-compatibility"))]
 					<Module<T>>::mark_deployed(*address, None).expect("Genesis contract shouldn't fail");
@@ -295,8 +285,6 @@ decl_error! {
 		NumOutOfBound,
 		/// Storage exceeds max code size
 		StorageExceedsStorageLimit,
-		/// Unreserve failed
-		UnreserveFailed,
 		/// Contract development is not enabled
 		ContractDevelopmentNotEnabled,
 		/// Contract development is already enabled
@@ -305,6 +293,8 @@ decl_error! {
 		ContractAlreadyDeployed,
 		/// Contract exceeds max code size
 		ContractExceedsMaxCodeSize,
+		/// Storage usage exceeds storage limit
+		OutOfStorage,
 	}
 }
 
@@ -526,7 +516,9 @@ decl_module! {
 
 impl<T: Config> Module<T> {
 	/// Remove an account.
-	pub fn remove_account(address: &EvmAddress) -> Result<(), ExitError> {
+	pub fn remove_account(address: &EvmAddress) -> Result<u32, ExitError> {
+		let mut size = 0u32;
+
 		// Deref code, and remove it if ref count is zero.
 		if let Some(AccountInfo {
 			contract_info: Some(contract_info),
@@ -535,6 +527,7 @@ impl<T: Config> Module<T> {
 		{
 			CodeInfos::mutate_exists(&contract_info.code_hash, |maybe_code_info| {
 				if let Some(code_info) = maybe_code_info.as_mut() {
+					size = code_info.code_size;
 					code_info.ref_count = code_info.ref_count.saturating_sub(1);
 					if code_info.ref_count == 0 {
 						Codes::remove(&contract_info.code_hash);
@@ -547,7 +540,7 @@ impl<T: Config> Module<T> {
 		<Accounts<T>>::remove(address);
 		AccountStorages::remove_prefix(address);
 
-		Ok(())
+		Ok(size)
 	}
 
 	/// Get the account basic in EVM format.
@@ -584,22 +577,15 @@ impl<T: Config> Module<T> {
 	/// Handler on new contract initialization.
 	///
 	/// - Create new account for the contract.
-	///   - For contracts initialized in genesis block, `storage_count` param
-	///     needed to be provided.
-	///   - For contracts initialized via dispatch calls, storage count would be
-	///     read from initialized account storages.
 	/// - Update codes info.
 	/// - Save `code` if not saved yet.
 	pub fn on_contract_initialization(
 		address: &EvmAddress,
 		maintainer: &EvmAddress,
 		code: Vec<u8>,
-		storage_count: Option<u32>,
 	) -> Result<(), ExitError> {
 		let code_hash = code_hash(&code.as_slice());
-		let storage_count = storage_count.unwrap_or_else(|| AccountStorages::iter_prefix(address).count() as u32);
 		let contract_info = ContractInfo {
-			storage_count,
 			code_hash,
 			maintainer: *maintainer,
 			#[cfg(feature = "with-ethereum-compatibility")]
@@ -638,64 +624,6 @@ impl<T: Config> Module<T> {
 		Ok(())
 	}
 
-	/// Set account storage.
-	pub fn set_storage(address: EvmAddress, index: H256, value: H256) -> Result<(), ExitError> {
-		enum StorageChange {
-			None,
-			Added,
-			Removed,
-		}
-
-		let mut storage_change = StorageChange::None;
-
-		let default_value = H256::default();
-		let is_prev_value_default = Self::account_storages(address, index) == default_value;
-
-		if value == default_value {
-			if !is_prev_value_default {
-				storage_change = StorageChange::Removed;
-			}
-
-			AccountStorages::remove(address, index);
-		} else {
-			if is_prev_value_default {
-				storage_change = StorageChange::Added;
-			}
-
-			AccountStorages::insert(address, index, value);
-		}
-
-		<Accounts<T>>::mutate(&address, |maybe_account_info| -> Result<(), ExitError> {
-			if let Some(AccountInfo {
-				contract_info: Some(contract_info),
-				..
-			}) = maybe_account_info.as_mut()
-			{
-				match storage_change {
-					StorageChange::Added => {
-						contract_info.storage_count = contract_info.storage_count.saturating_add(1);
-					}
-					StorageChange::Removed => {
-						contract_info.storage_count = contract_info.storage_count.saturating_sub(1);
-					}
-					_ => (),
-				}
-			}
-			Ok(())
-		})
-	}
-
-	/// Get additional storage of the contract.
-	fn storage_usage(contract: EvmAddress) -> u32 {
-		Accounts::<T>::get(contract).map_or(0, |account_info| {
-			account_info
-				.contract_info
-				.map_or(AccountStorages::iter_prefix(contract).count() as u32, |contract_info| {
-					contract_info.total_storage_size()
-				})
-		})
-	}
-
 	fn do_transfer_maintainer(who: T::AccountId, contract: EvmAddress, new_maintainer: EvmAddress) -> DispatchResult {
 		Accounts::<T>::get(contract).map_or(Err(Error::<T>::ContractNotFound), |account_info| {
 			account_info
@@ -732,14 +660,6 @@ impl<T: Config> Module<T> {
 			{
 				if let Some(maintainer) = maintainer {
 					ensure!(contract_info.maintainer == maintainer, Error::<T>::NoPermission);
-
-					let from = T::AddressMapping::get_account_id(&maintainer);
-					let to = T::AddressMapping::get_account_id(&contract);
-					let code_size = CodeInfos::get(contract_info.code_hash).map_or(0, |code_info| code_info.code_size);
-					let deposit_amount: BalanceOf<T> = T::StorageDepositPerByte::get()
-						.saturating_mul(T::NewContractExtraBytes::get().saturating_add(code_size).into());
-					T::Currency::transfer(&from, &to, deposit_amount, ExistenceRequirement::AllowDeath)?;
-					T::Currency::reserve(&to, deposit_amount)?;
 				}
 				ensure!(!contract_info.deployed, Error::<T>::ContractAlreadyDeployed);
 				contract_info.deployed = true;
--- modules/evm/src/runner/storage_meter.rs
+++ modules/evm/src/runner/storage_meter.rs
@@ -1,159 +1,413 @@
-use evm::ExitError;
+use frame_support::debug;
+use sp_core::H160;
+use sp_runtime::{DispatchError, DispatchResult};
 
-/// Storagemeter.
-#[derive(Clone, Debug)]
-pub struct Storagemeter {
-	storage_limit: u32,
-	inner: Result<Inner, ExitError>,
+pub trait StorageMeterHandler {
+	fn reserve_storage(&mut self, limit: u32) -> DispatchResult;
+	fn unreserve_storage(&mut self, limit: u32, used: u32, refunded: u32) -> DispatchResult;
+
+	fn charge_storage(&mut self, contract: &H160, used: u32, refunded: u32) -> DispatchResult;
+
+	fn out_of_storage_error(&self) -> DispatchError;
 }
 
-#[derive(Clone, Debug)]
-struct Inner {
-	used_storage: u32,
-	refunded_storage: u32,
+pub struct StorageMeter<'handler> {
+	contract: H160,
+	limit: u32,
+	total_used: u32,
+	self_used: u32,
+	total_refunded: u32,
+	self_refunded: u32,
+	handler: &'handler mut dyn StorageMeterHandler,
+	result: DispatchResult,
 }
 
-impl Storagemeter {
-	/// Create a new storagemeter with given storage limit.
-	pub fn new(storage_limit: u32) -> Self {
-		Self {
-			storage_limit,
-			inner: Ok(Inner {
-				used_storage: 0,
-				refunded_storage: 0,
-			}),
-		}
+impl<'handler> StorageMeter<'handler> {
+	/// Create a new storage_meter with given storage limit.
+	pub fn new(
+		handler: &'handler mut dyn StorageMeterHandler,
+		contract: H160,
+		limit: u32,
+	) -> Result<Self, DispatchError> {
+		debug::trace!(
+			target: "evm",
+			"StorageMeter: create: contract {:?} limit {:?}",
+			contract, limit
+		);
+		handler.reserve_storage(limit)?;
+		Ok(Self {
+			contract,
+			limit,
+			total_used: 0,
+			self_used: 0,
+			total_refunded: 0,
+			self_refunded: 0,
+			handler,
+			result: Ok(()),
+		})
 	}
 
-	fn inner_mut(&mut self) -> Result<&mut Inner, ExitError> {
-		self.inner.as_mut().map_err(|e| e.clone())
+	pub fn child_meter(&mut self, contract: H160) -> Result<StorageMeter<'_>, DispatchError> {
+		self.handle(|this| {
+			let storage = this.available_storage();
+			// can't make this.result = Err if `new` fails
+			// because some rust lifetime thing never happy
+			StorageMeter::new(this, contract, storage)
+		})
 	}
 
-	/// Remaining storage.
-	pub fn storage(&self) -> u32 {
-		match self.inner.as_ref() {
-			Ok(inner) => self.storage_limit - inner.used_storage + inner.refunded_storage,
-			Err(_) => 0,
-		}
-	}
-
-	/// Total used gas.
-	pub fn total_used_storage(&self) -> u32 {
-		match self.inner.as_ref() {
-			Ok(inner) => inner.used_storage,
-			Err(_) => self.storage_limit,
-		}
-	}
-
-	/// Refunded storage.
-	pub fn refunded_storage(&self) -> u32 {
-		match self.inner.as_ref() {
-			Ok(inner) => inner.refunded_storage,
-			Err(_) => 0,
-		}
-	}
-
-	/// Record an explict cost.
-	pub fn record_cost(&mut self, cost: u32) -> Result<(), ExitError> {
-		let all_storage_cost = self.total_used_storage() + cost;
-		if self.storage_limit < all_storage_cost {
-			self.inner = Err(ExitError::Other("OutOfStorageLimit".into()));
-			return Err(ExitError::Other("OutOfStorageLimit".into()));
-		}
-
-		self.inner_mut()?.used_storage += cost;
-		Ok(())
-	}
-
-	/// Record an explict refund.
-	pub fn record_refund(&mut self, refund: u32) -> Result<(), ExitError> {
-		self.inner_mut()?.refunded_storage += refund;
-		Ok(())
-	}
-
-	/// Record an explict stipend.
-	pub fn record_stipend(&mut self, stipend: u32) -> Result<(), ExitError> {
-		let inner = self.inner_mut()?;
-
-		if inner.used_storage > stipend {
-			inner.used_storage -= stipend;
+	pub fn available_storage(&self) -> u32 {
+		if self.result.is_ok() {
+			self.limit
+				.saturating_add(self.total_refunded)
+				.saturating_sub(self.total_used)
 		} else {
-			// stipend > used_storage
-			inner.refunded_storage += stipend - inner.used_storage;
-			inner.used_storage = 0;
+			0
 		}
+	}
+
+	pub fn used_storage(&self) -> i32 {
+		if self.total_used > self.total_refunded {
+			(self.total_used - self.total_refunded) as i32
+		} else {
+			-((self.total_refunded - self.total_used) as i32)
+		}
+	}
+
+	pub fn charge(&mut self, storage: u32) -> DispatchResult {
+		debug::trace!(
+			target: "evm",
+			"StorageMeter: charge: storage {:?}",
+			storage
+		);
+		self.handle(|this| {
+			let used = this.total_used.saturating_add(storage);
+			if this.limit < used.saturating_sub(this.total_refunded) {
+				this.result = Err(this.out_of_storage_error());
+				return this.result;
+			}
+			this.total_used = used;
+			this.self_used = this.self_used.saturating_add(storage);
+			Ok(())
+		})
+	}
+
+	pub fn uncharge(&mut self, storage: u32) -> DispatchResult {
+		debug::trace!(
+			target: "evm",
+			"StorageMeter: uncharge: storage {:?}",
+			storage
+		);
+		self.handle(|this| {
+			this.total_used = this.total_used.saturating_sub(storage);
+			this.self_used = this.self_used.saturating_sub(storage);
+			Ok(())
+		})
+	}
+
+	pub fn refund(&mut self, storage: u32) -> DispatchResult {
+		debug::trace!(
+			target: "evm",
+			"StorageMeter: refund: storage {:?}",
+			storage
+		);
+		self.handle(|this| {
+			this.total_refunded = this.total_refunded.saturating_add(storage);
+			this.self_refunded = this.self_refunded.saturating_add(storage);
+			Ok(())
+		})
+	}
+
+	pub fn finish(mut self) -> DispatchResult {
+		debug::trace!(
+			target: "evm",
+			"StorageMeter: finish: used {:?} refunded {:?}",
+			self.total_used, self.total_refunded
+		);
+		self.handle(|this| {
+			if let Err(x) = (|| {
+				this.handler
+					.charge_storage(&this.contract, this.self_used, this.self_refunded)?;
+				let new_limit = this
+					.limit
+					.saturating_add(this.self_used)
+					.saturating_add(this.total_refunded)
+					.saturating_sub(this.total_used)
+					.saturating_sub(this.self_refunded);
+				this.handler
+					.unreserve_storage(new_limit, this.self_used, this.self_refunded)
+			})() {
+				this.result = Err(x);
+				Err(x)
+			} else {
+				Ok(())
+			}
+		})
+	}
+
+	fn handle<'a, R, F: FnOnce(&'a mut Self) -> Result<R, DispatchError>>(
+		&'a mut self,
+		f: F,
+	) -> Result<R, DispatchError> {
+		self.result?;
+		f(self)
+	}
+}
+
+impl<'handler> StorageMeterHandler for StorageMeter<'handler> {
+	fn reserve_storage(&mut self, _limit: u32) -> DispatchResult {
 		Ok(())
+	}
+
+	fn unreserve_storage(&mut self, _limit: u32, _used: u32, _refunded: u32) -> DispatchResult {
+		Ok(())
+	}
+
+	fn charge_storage(&mut self, contract: &H160, used: u32, refunded: u32) -> DispatchResult {
+		self.handle(|this| {
+			this.total_refunded = this.total_refunded.saturating_add(refunded);
+			this.total_used = this.total_used.saturating_add(used);
+			this.handler.charge_storage(contract, used, refunded)
+		})
+	}
+
+	fn out_of_storage_error(&self) -> DispatchError {
+		"OutOfStorage".into()
 	}
 }
 
 #[cfg(test)]
 mod tests {
 	use super::*;
-	use frame_support::assert_ok;
+	use frame_support::{assert_err, assert_ok};
 
-	#[test]
-	fn test_storage_with_limit_zero() {
-		let mut storagemeter = Storagemeter::new(0);
-		assert_eq!(storagemeter.storage(), 0);
-		assert_eq!(storagemeter.refunded_storage(), 0);
-		assert_eq!(storagemeter.total_used_storage(), 0);
+	const ALICE: H160 = H160::repeat_byte(11);
+	const CONTRACT: H160 = H160::repeat_byte(22);
+	const CONTRACT_2: H160 = H160::repeat_byte(33);
+	const CONTRACT_3: H160 = H160::repeat_byte(44);
+	struct DummyHandler {
+		pub storages: std::collections::BTreeMap<H160, u32>,
+		pub reserves: std::collections::BTreeMap<H160, u32>,
+	}
 
-		// record_refund
-		assert_ok!(storagemeter.record_refund(1));
-		assert_eq!(storagemeter.storage(), 1);
-		assert_eq!(storagemeter.refunded_storage(), 1);
-		assert_eq!(storagemeter.total_used_storage(), 0);
+	impl DummyHandler {
+		fn new() -> Self {
+			let mut val = Self {
+				storages: Default::default(),
+				reserves: Default::default(),
+			};
+			val.storages.insert(ALICE, 0);
+			val.reserves.insert(ALICE, 0);
+			val.storages.insert(CONTRACT, 0);
+			val.reserves.insert(CONTRACT, 0);
+			val.storages.insert(CONTRACT_2, 0);
+			val.reserves.insert(CONTRACT_2, 0);
+			val.storages.insert(CONTRACT_3, 0);
+			val.reserves.insert(CONTRACT_3, 0);
+			val
+		}
+	}
 
-		// record_stipend
-		assert_ok!(storagemeter.record_stipend(1));
-		assert_eq!(storagemeter.storage(), 2);
-		assert_eq!(storagemeter.refunded_storage(), 2);
-		assert_eq!(storagemeter.total_used_storage(), 0);
+	impl StorageMeterHandler for DummyHandler {
+		fn reserve_storage(&mut self, limit: u32) -> DispatchResult {
+			if limit == 0 {
+				return Ok(());
+			}
+			let val = self.storages.get_mut(&ALICE).ok_or("error")?;
+			*val = val.checked_sub(limit).ok_or("error")?;
+			if let Some(v) = self.reserves.get_mut(&ALICE) {
+				*v += limit;
+			} else {
+				self.reserves.insert(ALICE, limit);
+			}
+			Ok(())
+		}
 
-		// record_cost
-		assert_eq!(
-			storagemeter.record_cost(1),
-			Err(ExitError::Other("OutOfStorageLimit".into()))
-		);
+		fn unreserve_storage(&mut self, limit: u32, used: u32, refunded: u32) -> DispatchResult {
+			let diff = limit + refunded - used;
+			if diff == 0 {
+				return Ok(());
+			}
+			let val = self.reserves.get_mut(&ALICE).ok_or("error")?;
+			*val = val.checked_sub(diff).ok_or("error")?;
+			if let Some(v) = self.storages.get_mut(&ALICE) {
+				*v += diff;
+			} else {
+				self.storages.insert(ALICE, diff);
+			}
+			Ok(())
+		}
+
+		fn charge_storage(&mut self, contract: &H160, used: u32, refunded: u32) -> DispatchResult {
+			if used == refunded {
+				return Ok(());
+			}
+			let alice = self.reserves.get_mut(&ALICE).ok_or("error")?;
+			if used > refunded {
+				*alice = alice.checked_sub(used - refunded).ok_or("error")?;
+			} else {
+				*alice = alice.checked_add(refunded - used).ok_or("error")?;
+			}
+
+			let contract_val = self.reserves.get_mut(&contract).ok_or("error")?;
+			if used > refunded {
+				*contract_val = contract_val.checked_add(used - refunded).ok_or("error")?;
+			} else {
+				*contract_val = contract_val.checked_sub(refunded - used).ok_or("error")?;
+			}
+			Ok(())
+		}
+
+		fn out_of_storage_error(&self) -> DispatchError {
+			"OutOfStorage".into()
+		}
 	}
 
 	#[test]
-	fn test_storage_with_limit_ten() {
-		let mut storagemeter = Storagemeter::new(10);
-		assert_eq!(storagemeter.storage(), 10);
-		assert_eq!(storagemeter.refunded_storage(), 0);
-		assert_eq!(storagemeter.total_used_storage(), 0);
+	fn test_storage_with_limit_zero() {
+		let mut handler = DummyHandler::new();
 
-		// record_refund
-		assert_ok!(storagemeter.record_refund(1));
-		assert_eq!(storagemeter.storage(), 11);
-		assert_eq!(storagemeter.refunded_storage(), 1);
-		assert_eq!(storagemeter.total_used_storage(), 0);
+		let mut storage_meter = StorageMeter::new(&mut handler, CONTRACT, 0).unwrap();
+		assert_eq!(storage_meter.available_storage(), 0);
 
-		// record_cost
-		assert_ok!(storagemeter.record_cost(1));
-		assert_eq!(storagemeter.storage(), 10);
-		assert_eq!(storagemeter.refunded_storage(), 1);
-		assert_eq!(storagemeter.total_used_storage(), 1);
+		// refund
+		assert_ok!(storage_meter.refund(1));
+		assert_eq!(storage_meter.available_storage(), 1);
 
-		// record_stipend
-		assert_ok!(storagemeter.record_stipend(1));
-		assert_eq!(storagemeter.storage(), 11);
-		assert_eq!(storagemeter.refunded_storage(), 1);
-		assert_eq!(storagemeter.total_used_storage(), 0);
+		// charge
+		assert_ok!(storage_meter.charge(1));
+		assert_eq!(storage_meter.available_storage(), 0);
 
-		// record_cost
-		assert_eq!(
-			storagemeter.record_cost(11),
-			Err(ExitError::Other("OutOfStorageLimit".into()))
+		assert_ok!(storage_meter.finish());
+
+		assert_eq!(handler.storages.get(&ALICE).cloned().unwrap_or_default(), 0);
+		assert_eq!(handler.reserves.get(&ALICE).cloned().unwrap_or_default(), 0);
+		assert_eq!(handler.storages.get(&CONTRACT).cloned().unwrap_or_default(), 0);
+		assert_eq!(handler.reserves.get(&CONTRACT).cloned().unwrap_or_default(), 0);
+	}
+
+	#[test]
+	fn test_charge_storage_fee() {
+		let mut handler = DummyHandler::new();
+		handler.storages.insert(ALICE, 1000);
+
+		let mut storage_meter = StorageMeter::new(&mut handler, CONTRACT, 1000).unwrap();
+		assert_eq!(storage_meter.available_storage(), 1000);
+
+		assert_ok!(storage_meter.refund(1));
+		assert_eq!(storage_meter.available_storage(), 1001);
+
+		assert_ok!(storage_meter.charge(101));
+		assert_eq!(storage_meter.available_storage(), 900);
+
+		assert_ok!(storage_meter.charge(50));
+		assert_eq!(storage_meter.available_storage(), 850);
+
+		assert_ok!(storage_meter.refund(20));
+		assert_eq!(storage_meter.available_storage(), 870);
+
+		assert_ok!(storage_meter.finish());
+
+		assert_eq!(handler.storages.get(&ALICE).cloned().unwrap_or_default(), 870);
+		assert_eq!(handler.reserves.get(&ALICE).cloned().unwrap_or_default(), 0);
+		assert_eq!(handler.storages.get(&CONTRACT).cloned().unwrap_or_default(), 0);
+		assert_eq!(handler.reserves.get(&CONTRACT).cloned().unwrap_or_default(), 130);
+	}
+
+	#[test]
+	fn test_refund_storage_fee() {
+		let mut handler = DummyHandler::new();
+		handler.storages.insert(ALICE, 1000);
+		handler.reserves.insert(CONTRACT, 1000);
+
+		let mut storage_meter = StorageMeter::new(&mut handler, CONTRACT, 1000).unwrap();
+		assert_eq!(storage_meter.available_storage(), 1000);
+
+		assert_ok!(storage_meter.refund(100));
+		assert_eq!(storage_meter.available_storage(), 1100);
+
+		assert_ok!(storage_meter.charge(50));
+		assert_eq!(storage_meter.available_storage(), 1050);
+
+		assert_ok!(storage_meter.finish());
+
+		assert_eq!(handler.storages.get(&ALICE).cloned().unwrap_or_default(), 1050);
+		assert_eq!(handler.reserves.get(&ALICE).cloned().unwrap_or_default(), 0);
+		assert_eq!(handler.storages.get(&CONTRACT).cloned().unwrap_or_default(), 0);
+		assert_eq!(handler.reserves.get(&CONTRACT).cloned().unwrap_or_default(), 950);
+	}
+
+	#[test]
+	fn test_out_of_storage() {
+		let mut handler = DummyHandler::new();
+		handler.storages.insert(ALICE, 1000);
+
+		assert!(StorageMeter::new(&mut handler, CONTRACT, 1001).is_err());
+		let mut storage_meter = StorageMeter::new(&mut handler, CONTRACT, 1000).unwrap();
+		assert_eq!(storage_meter.available_storage(), 1000);
+
+		assert_ok!(storage_meter.charge(500));
+		assert_eq!(storage_meter.available_storage(), 500);
+
+		assert_ok!(storage_meter.charge(500));
+		assert_eq!(storage_meter.available_storage(), 0);
+
+		assert_err!(storage_meter.charge(1), DispatchError::Other("OutOfStorage"));
+		assert_err!(storage_meter.refund(1), DispatchError::Other("OutOfStorage"));
+		assert_err!(
+			storage_meter.child_meter(CONTRACT_2).map(|_| ()),
+			DispatchError::Other("OutOfStorage")
 		);
-		assert_eq!(
-			storagemeter.record_stipend(1),
-			Err(ExitError::Other("OutOfStorageLimit".into()))
-		);
-		assert_eq!(
-			storagemeter.record_refund(1),
-			Err(ExitError::Other("OutOfStorageLimit".into()))
-		);
+		assert_err!(storage_meter.finish(), DispatchError::Other("OutOfStorage"));
+	}
+
+	#[test]
+	fn test_child_meter() {
+		let mut handler = DummyHandler::new();
+		handler.storages.insert(ALICE, 1000);
+
+		let mut storage_meter = StorageMeter::new(&mut handler, CONTRACT, 1000).unwrap();
+
+		assert_ok!(storage_meter.charge(100));
+
+		let mut child_meter = storage_meter.child_meter(CONTRACT_2).unwrap();
+		assert_eq!(child_meter.available_storage(), 900);
+
+		assert_ok!(child_meter.charge(100));
+		assert_eq!(child_meter.available_storage(), 800);
+
+		assert_ok!(child_meter.refund(50));
+		assert_eq!(child_meter.available_storage(), 850);
+
+		let mut child_meter_2 = child_meter.child_meter(CONTRACT_3).unwrap();
+
+		assert_eq!(child_meter_2.available_storage(), 850);
+
+		assert_ok!(child_meter_2.charge(20));
+		assert_eq!(child_meter_2.available_storage(), 830);
+
+		assert_ok!(child_meter_2.finish());
+
+		assert_ok!(child_meter.finish());
+
+		let mut child_meter_3 = storage_meter.child_meter(CONTRACT_2).unwrap();
+
+		assert_eq!(child_meter_3.available_storage(), 830);
+
+		assert_ok!(child_meter_3.charge(30));
+
+		assert_eq!(child_meter_3.available_storage(), 800);
+
+		assert_ok!(child_meter_3.finish());
+
+		assert_eq!(storage_meter.available_storage(), 800);
+
+		assert_ok!(storage_meter.finish());
+
+		assert_eq!(handler.storages.get(&ALICE).cloned().unwrap_or_default(), 800);
+		assert_eq!(handler.reserves.get(&ALICE).cloned().unwrap_or_default(), 0);
+		assert_eq!(handler.reserves.get(&CONTRACT).cloned().unwrap_or_default(), 100);
+		assert_eq!(handler.reserves.get(&CONTRACT_2).cloned().unwrap_or_default(), 80);
+		assert_eq!(handler.reserves.get(&CONTRACT_3).cloned().unwrap_or_default(), 20);
 	}
 }
