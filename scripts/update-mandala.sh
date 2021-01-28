eeae8942d226632024a968cac992f43a2fd49a34
Bryan Chen <xlchen1291@gmail.com>
2021-01-29 12:24:19 +1300

    update TC6 genesis

--- scripts/update-mandala.sh
+++ scripts/update-mandala.sh
@@ -3,5 +3,5 @@
 set -e
 
 # cargo clean
-WASM_BUILD_TYPE=release cargo run -- build-spec --chain mandala-latest > ./resources/mandala.json
-WASM_BUILD_TYPE=release cargo run -- build-spec --chain ./resources/mandala.json --raw > ./resources/mandala-dist.json
+WASM_BUILD_TYPE=release cargo run --manifest-path bin/acala-dev/Cargo.toml --features with-ethereum-compatibility -- build-spec --chain mandala-latest > ./resources/mandala.json
+WASM_BUILD_TYPE=release cargo run --manifest-path bin/acala-dev/Cargo.toml --features with-ethereum-compatibility -- build-spec --chain ./resources/mandala.json --raw > ./resources/mandala-dist.json
