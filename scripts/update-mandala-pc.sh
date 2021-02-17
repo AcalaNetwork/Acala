a2a28d8204a5579e83a09910b0c372ea782cbae7
Shaopeng Wang <spxwang@gmail.com>
2021-02-17 13:19:43 +1300

    Update mandala pc script and spec json.

--- scripts/update-mandala-pc.sh
+++ scripts/update-mandala-pc.sh
@@ -3,5 +3,4 @@
 set -e
 
 # cargo clean
-WASM_BUILD_TYPE=release cargo run --manifest-path bin/acala/Cargo.toml -- build-spec --chain mandala-latest > ./resources/mandala-pc.json
-WASM_BUILD_TYPE=release cargo run --manifest-path bin/acala/Cargo.toml -- build-spec --chain ./resources/mandala-pc.json --raw > ./resources/mandala-pc-dist.json
+WASM_BUILD_TYPE=release cargo run --manifest-path bin/acala/Cargo.toml -- build-spec --chain mandala-latest --raw > ./resources/mandala-pc-dist.json
