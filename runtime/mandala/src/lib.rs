ac16e6222d17ea693ac3779c19471b8bce02fb9b
Shaopeng Wang <spxwang@gmail.com>
2021-02-18 14:05:49 +1300

    Bump spec version.

--- runtime/mandala/src/lib.rs
+++ runtime/mandala/src/lib.rs
@@ -95,7 +95,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("mandala"),
 	impl_name: create_runtime_str!("mandala"),
 	authoring_version: 1,
-	spec_version: 709,
+	spec_version: 710,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
