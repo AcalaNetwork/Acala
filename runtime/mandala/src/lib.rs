ac5944918db5b14f0412bf3a5fa45f30814aa7ff
Bryan Chen <xlchen1291@gmail.com>
2021-03-03 12:13:52 +1300

    update version

--- runtime/mandala/src/lib.rs
+++ runtime/mandala/src/lib.rs
@@ -95,7 +95,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("mandala"),
 	impl_name: create_runtime_str!("mandala"),
 	authoring_version: 1,
-	spec_version: 711,
+	spec_version: 720,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
