4fa1bf679d772fb85b1e7908b4ac2a76c9cfe3b0
Bryan Chen <xlchen1291@gmail.com>
2021-03-12 12:27:19 +1300

    update docker file

--- scripts/docker-hub-publish.sh
+++ scripts/docker-hub-publish.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 VERSION=$1
 
 if [[ -z "$1" ]] ; then
@@ -7,6 +9,6 @@ if [[ -z "$1" ]] ; then
     exit 1
 fi
 
-docker build . -t acala/acala-node:$1 -t acala/acala-node:latest
+docker build -f scripts/Dockerfile-dev . -t acala/acala-node:$1 -t acala/acala-node:latest
 docker push acala/acala-node:$1
 docker push acala/acala-node:latest
