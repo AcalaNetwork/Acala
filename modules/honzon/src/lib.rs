7c4f9cabbd42cb054f97693ef190d50e4dc78772
wangjj9219 <183318287@qq.com>
2021-04-14 23:37:39 +0800

    fix

--- modules/honzon/src/lib.rs
+++ modules/honzon/src/lib.rs
@@ -127,7 +127,7 @@ pub mod module {
 		pub fn close_loan_has_debit_by_dex(
 			origin: OriginFor<T>,
 			currency_id: CurrencyId,
-			maybe_path: Option<Vec<CurrencyId>>,
+			maybe_path: Option<sp_std::vec::Vec<CurrencyId>>,
 		) -> DispatchResultWithPostInfo {
 			let who = ensure_signed(origin)?;
 			ensure!(!T::EmergencyShutdown::is_shutdown(), Error::<T>::AlreadyShutdown);
