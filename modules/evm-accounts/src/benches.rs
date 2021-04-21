02a48f3e5cdc29c00fd8c9fd5dc305466d1c2023
Bryan Chen <xlchen1291@gmail.com>
2021-04-22 11:07:05 +1200

    fix get_or_create_evm_address

--- modules/evm-accounts/src/benches.rs
+++ modules/evm-accounts/src/benches.rs
@@ -23,6 +23,7 @@
 pub use crate::mock::{for_bench::Block, wasm_binary_unwrap};
 
 use crate::mock::for_bench::{alice, EvmAccountsModule, Runtime, ALICE};
+use module_support::AddressMapping;
 
 fn eth_address(b: &mut Bencher) {
 	b.bench("eth_address", || {
@@ -48,10 +49,10 @@ fn eth_public(b: &mut Bencher) {
 	});
 }
 
-// fn get_or_create_evm_address(b: &mut Bencher) {
-//     b.bench("get_or_create_evm_address", || {
-//         EvmAddressMapping::<Runtime>::get_or_create_evm_address(&ALICE);
-//     });
-// }
+fn get_or_create_evm_address(b: &mut Bencher) {
+	b.bench("get_or_create_evm_address", || {
+		crate::EvmAddressMapping::<Runtime>::get_or_create_evm_address(&ALICE);
+	});
+}
 
 orml_bencher::bench!(eth_address, eth_sign, ethereum_signable_message, eth_public);
