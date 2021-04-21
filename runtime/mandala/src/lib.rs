8ca3a2b3aa89bfa998438b53d83636953c1ccfb4
Xiliang Chen <xlchen1291@gmail.com>
2021-04-21 23:05:51 +1200

    Bump (#943)
    * update docker base image
    
    * bump version

--- runtime/mandala/src/lib.rs
+++ runtime/mandala/src/lib.rs
@@ -145,7 +145,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("mandala"),
 	impl_name: create_runtime_str!("mandala"),
 	authoring_version: 1,
-	spec_version: 730,
+	spec_version: 800,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
