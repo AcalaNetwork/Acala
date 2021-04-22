4fb4cd45e8fc173bf2648c1b2fdcb1cfde73391e
Bryan Chen <xlchen1291@gmail.com>
2021-04-22 15:04:21 +1200

    update block weight

--- runtime/karura/src/lib.rs
+++ runtime/karura/src/lib.rs
@@ -103,9 +103,8 @@ pub use primitives::{
 	BlockNumber, CurrencyId, DataProviderId, EraIndex, Hash, Moment, Nonce, Share, Signature, TokenSymbol, TradingPair,
 };
 pub use runtime_common::{
-	cent, deposit, dollar, microcent, millicent, CurveFeeModel, ExchangeRate, GasToWeight, OffchainSolutionWeightLimit,
-	Price, Rate, Ratio, RuntimeBlockLength, RuntimeBlockWeights, SystemContractsFilter, TimeStampedPrice, KAR, KSM,
-	KUSD, LKSM, RENBTC,
+	cent, deposit, dollar, microcent, millicent, CurveFeeModel, ExchangeRate, GasToWeight, Price, Rate, Ratio,
+	RuntimeBlockLength, RuntimeBlockWeights, SystemContractsFilter, TimeStampedPrice, KAR, KSM, KUSD, LKSM, RENBTC,
 };
 
 mod authority;
