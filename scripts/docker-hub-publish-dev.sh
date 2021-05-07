dec37acf0fb76bbeabd182edc289fc079bced31c
Shaun Wang <spxwang@gmail.com>
2021-05-07 13:00:45 +1200

    Fix dockerfile.

--- scripts/docker-hub-publish-dev.sh
+++ scripts/docker-hub-publish-dev.sh
@@ -4,5 +4,5 @@ set -e
 
 VERSION=$(git rev-parse --short HEAD)
 
-docker build -f scripts/Dockerfile-dev . -t acala/acala-node:$VERSION --no-cache --build-arg GIT_COMMIT=${VERSION}
+docker build -f scripts/Dockerfile . -t acala/acala-node:$VERSION --no-cache --build-arg GIT_COMMIT=${VERSION} --build-arg CARGO_BUILD_ARGS=--features=with-ethereum-compatibility
 docker push acala/acala-node:$VERSION
--- scripts/docker-hub-publish-parachain-dev.sh
+++ scripts/docker-hub-publish-parachain-dev.sh
@@ -4,5 +4,5 @@ set -e
 
 VERSION=$(git rev-parse --short HEAD)
 
-docker build -f scripts/Dockerfile-parachain . -t acala/acala-node:pc-$VERSION --no-cache --build-arg GIT_COMMIT=${VERSION}
+docker build -f scripts/Dockerfile . -t acala/acala-node:pc-$VERSION --no-cache --build-arg GIT_COMMIT=${VERSION}
 docker push acala/acala-node:pc-$VERSION
