47563928c250c1cf26538dce4ea00c4034e5f2fa
Bryan Chen <xlchen1291@gmail.com>
2021-05-10 13:09:44 +1200

    update mandala script

--- scripts/update-mandala.sh
+++ scripts/update-mandala.sh
@@ -3,4 +3,4 @@
 set -e
 
 # cargo clean
-WASM_BUILD_TYPE=release cargo run --manifest-path bin/acala-dev/Cargo.toml --features with-ethereum-compatibility -- build-spec --raw --chain mandala-latest > ./resources/mandala-dist.json
+WASM_BUILD_TYPE=release cargo run --features with-ethereum-compatibility -- build-spec --raw --chain mandala-latest > ./resources/mandala-dist.json
