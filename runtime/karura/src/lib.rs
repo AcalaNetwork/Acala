8bd5d408a0e070ee1590e3b55d0556ebab7c6a23
wangjj9219 <183318287@qq.com>
2021-05-14 15:40:03 +0800

    update KAR localtion if genesis (#1003)

--- runtime/karura/src/lib.rs
+++ runtime/karura/src/lib.rs
@@ -286,7 +286,7 @@ impl pallet_timestamp::Config for Runtime {
 }
 
 parameter_types! {
-	pub const NativeTokenExistentialDeposit: Balance = 0;
+	pub NativeTokenExistentialDeposit: Balance = 100 * millicent(KAR);	// 0.1 KAR
 	// For weight estimation, we assume that the most locks on an individual account will be 50.
 	// This number may need to be adjusted in the future if this assumption no longer holds true.
 	pub const MaxLocks: u32 = 50;
