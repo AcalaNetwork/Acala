5b59563a15006ece226dff0749e9cb63246df167
Bryan Chen <xlchen1291@gmail.com>
2021-05-14 22:10:08 +1200

    update

--- node/service/src/chain_spec/karura.rs
+++ node/service/src/chain_spec/karura.rs
@@ -23,14 +23,13 @@ use sc_telemetry::TelemetryEndpoints;
 use serde_json::map::Map;
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_core::crypto::UncheckedInto;
-use sp_finality_grandpa::AuthorityId as GrandpaId;
 use sp_runtime::traits::Zero;
 
 use crate::chain_spec::{Extensions, TELEMETRY_URL};
 
 pub type ChainSpec = sc_service::GenericChainSpec<karura_runtime::GenesisConfig, Extensions>;
 
-pub const PARA_ID: u32 = 1000;
+pub const PARA_ID: u32 = 2000;
 
 pub fn karura_config() -> Result<ChainSpec, String> {
 	Err("Not available".into())
@@ -50,21 +49,9 @@ pub fn latest_karura_config() -> Result<ChainSpec, String> {
 	let wasm_binary = karura_runtime::WASM_BINARY.ok_or("Karura runtime wasm binary not available")?;
 
 	Ok(ChainSpec::from_genesis(
-		"Acala Karura",
+		"Karura",
 		"karura",
 		ChainType::Live,
-		// SECRET="..."
-		// ./target/debug/subkey inspect "$SECRET//acala//root"
-		// ./target/debug/subkey --sr25519 inspect "$SECRET//acala//oracle"
-		// ./target/debug/subkey --sr25519 inspect "$SECRET//acala//1//validator"
-		// ./target/debug/subkey --sr25519 inspect "$SECRET//acala//1//babe"
-		// ./target/debug/subkey --ed25519 inspect "$SECRET//acala//1//grandpa"
-		// ./target/debug/subkey --sr25519 inspect "$SECRET//acala//2//validator"
-		// ./target/debug/subkey --sr25519 inspect "$SECRET//acala//2//babe"
-		// ./target/debug/subkey --ed25519 inspect "$SECRET//acala//2//grandpa"
-		// ./target/debug/subkey --sr25519 inspect "$SECRET//acala//3//validator"
-		// ./target/debug/subkey --sr25519 inspect "$SECRET//acala//3//babe"
-		// ./target/debug/subkey --ed25519 inspect "$SECRET//acala//3//grandpa"
 		move || {
 			karura_genesis(
 				wasm_binary,
@@ -72,22 +59,16 @@ pub fn latest_karura_config() -> Result<ChainSpec, String> {
 					(
 						// 5CLg63YpPJNqcyWaYebk3LuuUVp3un7y1tmuV3prhdbnMA77
 						hex!["0c2df85f943312fc853059336627d0b7a08669629ebd99b4debc6e58c1b35c2b"].into(),
-						hex!["0c2df85f943312fc853059336627d0b7a08669629ebd99b4debc6e58c1b35c2b"].into(),
-						hex!["21b5a771b99ef0f059c476502c018c4b817fb0e48858e95a238850d2b7828556"].unchecked_into(),
 						hex!["948f15728a5fd66e36503c048cc7b448cb360a825240c48ff3f89efe050de608"].unchecked_into(),
 					),
 					(
 						// 5FnLzAUmXeTZg5J9Ao5psKU68oA5PBekXqhrZCKDbhSCQi88
 						hex!["a476c0050065dafac1e9ff7bf602fe628ceadacf67650f8317554bd571b73507"].into(),
-						hex!["a476c0050065dafac1e9ff7bf602fe628ceadacf67650f8317554bd571b73507"].into(),
-						hex!["77f3c27e98da7849ed0749e1dea449321a4a5a36a1dccf3f08fc0ab3af24c62e"].unchecked_into(),
 						hex!["b4f5713322656d29930aa89efa5509554a36c40fb50a226eae0f38fc1a6ceb25"].unchecked_into(),
 					),
 					(
 						// 5Gn5LuLuWNcY21Vue4QcFFD3hLvjQY3weMHXuEyejUbUnArt
 						hex!["d07e538fee7c42be9b2627ea5caac9a30f1869d65af2a19df70138d5fcc34310"].into(),
-						hex!["d07e538fee7c42be9b2627ea5caac9a30f1869d65af2a19df70138d5fcc34310"].into(),
-						hex!["c5dfcf68ccf1a64ed4145383e4bbbb8bbcc50f654d87187c39df2b88a9683b7f"].unchecked_into(),
 						hex!["4cc54799f38715771605a21e8272a7a1344667e4681611988a913412755a8a04"].unchecked_into(),
 					),
 				],
@@ -96,16 +77,13 @@ pub fn latest_karura_config() -> Result<ChainSpec, String> {
 			)
 		},
 		vec![
-			//TODO
-			"/dns/testnet-bootnode-1.karura.laminar.one/tcp/30333/p2p/12D3KooWAFUNUowRqCV4c5so58Q8iGpypVf3L5ak91WrHf7rPuKz"
-				.parse()
-				.unwrap(),
+			// no bootnode yet
 		],
 		TelemetryEndpoints::new(vec![(TELEMETRY_URL.into(), 0)]).ok(),
 		Some("karura"),
 		Some(properties),
 		Extensions {
-			relay_chain: "rococo".into(),
+			relay_chain: "kusama".into(),
 			para_id: PARA_ID,
 		},
 	))
@@ -113,12 +91,12 @@ pub fn latest_karura_config() -> Result<ChainSpec, String> {
 
 fn karura_genesis(
 	wasm_binary: &[u8],
-	initial_authorities: Vec<(AccountId, AccountId, GrandpaId, AuraId)>,
+	initial_authorities: Vec<(AccountId, AuraId)>,
 	root_key: AccountId,
 ) -> karura_runtime::GenesisConfig {
 	use karura_runtime::{
 		dollar, get_all_module_accounts, AcalaOracleConfig, Balance, BalancesConfig, BlockNumber, CdpEngineConfig,
-		CdpTreasuryConfig, CollatorSelectionConfig, DexConfig, EnabledTradingPairs, GeneralCouncilMembershipConfig,
+		CdpTreasuryConfig, CollatorSelectionConfig, DexConfig, GeneralCouncilMembershipConfig,
 		HomaCouncilMembershipConfig, HonzonCouncilMembershipConfig, NativeTokenExistentialDeposit,
 		OperatorMembershipAcalaConfig, OrmlNFTConfig, ParachainInfoConfig, SessionConfig, SessionKeys, SudoConfig,
 		SystemConfig, TechnicalCommitteeMembershipConfig, TokensConfig, VestingConfig, KAR,
@@ -193,25 +171,25 @@ fn karura_genesis(
 		pallet_balances: BalancesConfig {
 			balances: initial_allocation,
 		},
-		pallet_sudo: SudoConfig { key: root_key.clone() },
+		pallet_sudo: SudoConfig { key: root_key },
 		pallet_collective_Instance1: Default::default(),
 		pallet_membership_Instance1: GeneralCouncilMembershipConfig {
-			members: vec![root_key.clone()],
+			members: vec![],
 			phantom: Default::default(),
 		},
 		pallet_collective_Instance2: Default::default(),
 		pallet_membership_Instance2: HonzonCouncilMembershipConfig {
-			members: vec![root_key.clone()],
+			members: vec![],
 			phantom: Default::default(),
 		},
 		pallet_collective_Instance3: Default::default(),
 		pallet_membership_Instance3: HomaCouncilMembershipConfig {
-			members: vec![root_key.clone()],
+			members: vec![],
 			phantom: Default::default(),
 		},
 		pallet_collective_Instance4: Default::default(),
 		pallet_membership_Instance4: TechnicalCommitteeMembershipConfig {
-			members: vec![root_key],
+			members: vec![],
 			phantom: Default::default(),
 		},
 		pallet_membership_Instance5: OperatorMembershipAcalaConfig {
@@ -237,7 +215,7 @@ fn karura_genesis(
 		module_evm: Default::default(),
 		module_dex: DexConfig {
 			initial_listing_trading_pairs: vec![],
-			initial_enabled_trading_pairs: EnabledTradingPairs::get(),
+			initial_enabled_trading_pairs: vec![],
 			initial_added_liquidity_pools: vec![],
 		},
 		parachain_info: ParachainInfoConfig {
@@ -245,7 +223,7 @@ fn karura_genesis(
 		},
 		orml_nft: OrmlNFTConfig { tokens: vec![] },
 		module_collator_selection: CollatorSelectionConfig {
-			invulnerables: initial_authorities.iter().cloned().map(|(acc, _, _, _)| acc).collect(),
+			invulnerables: initial_authorities.iter().cloned().map(|(acc, _)| acc).collect(),
 			candidacy_bond: Zero::zero(),
 			..Default::default()
 		},
@@ -253,7 +231,7 @@ fn karura_genesis(
 			keys: initial_authorities
 				.iter()
 				.cloned()
-				.map(|(acc, _, _, aura)| {
+				.map(|(acc, aura)| {
 					(
 						acc.clone(),          // account id
 						acc,                  // validator id
