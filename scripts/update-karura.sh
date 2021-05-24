2e2e5368c5f4c94e113d339327318eb607df0570
Bryan Chen <xlchen1291@gmail.com>
2021-05-24 00:32:17 +0000

    update Karura genesis

--- scripts/update-karura.sh
+++ scripts/update-karura.sh
@@ -3,4 +3,4 @@
 set -e
 
 # cargo clean
-WASM_BUILD_TYPE=release cargo run --features with-karura-runtime -- build-spec --raw --chain karura-latest > ./resources/karura-dist.json
+WASM_BUILD_TYPE=release cargo run --features=with-karura-runtime --features=on-chain-release-build -- build-spec --raw --chain karura-latest > ./resources/karura-dist.json
