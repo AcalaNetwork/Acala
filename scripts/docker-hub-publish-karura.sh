097cf4d99b8f044db6f52ad1de5f7a570e97e225
Xiliang Chen <xlchen1291@gmail.com>
2021-05-30 18:44:14 +1200

    update scripts (#1055)

--- scripts/docker-hub-publish-karura.sh
+++ scripts/docker-hub-publish-karura.sh
@@ -7,8 +7,7 @@ NODE_NAME=acala/karura-node
 BUILD_ARGS="--features with-karura-runtime"
 
 if [[ -z "$1" ]] ; then
-    echo "Usage: ./scripts/docker-hub-publish-karura.sh VERSION"
-    exit 1
+    VERSION=$(git rev-parse --short HEAD)
 fi
 
 docker build -f scripts/Dockerfile . -t $NODE_NAME:$VERSION -t $NODE_NAME:latest --build-arg GIT_COMMIT=${VERSION} --build-arg BUILD_ARGS="$BUILD_ARGS"
