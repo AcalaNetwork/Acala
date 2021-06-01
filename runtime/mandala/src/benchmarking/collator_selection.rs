720975785322a73ea068d6fc7be0bc16890cdfd6
zjb0807 <zjb0807@qq.com>
2021-06-01 11:59:02 +0800

    remove unused code

--- runtime/mandala/src/benchmarking/collator_selection.rs
+++ runtime/mandala/src/benchmarking/collator_selection.rs
@@ -16,9 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-use crate::{
-	AccountId, Balance, Balances, CollatorSelection, Event, MaxCandidates, MaxInvulnerables, Runtime, Session, System,
-};
+use crate::{AccountId, Balance, Balances, CollatorSelection, Event, MaxCandidates, MaxInvulnerables, Runtime, System};
 
 use frame_benchmarking::{account, whitelisted_caller};
 use frame_support::assert_ok;
