8f994df9a3b20c628ae7fa26cda6eb66c170dfe7
Bryan Chen <xlchen1291@gmail.com>
2021-06-19 18:13:10 +1200

    remove update karura script

--- scripts/update-karura.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-# cargo clean
-WASM_BUILD_TYPE=release cargo run --features=with-karura-runtime --features=on-chain-release-build -- build-spec --raw --chain karura-latest > ./resources/karura-dist.json
