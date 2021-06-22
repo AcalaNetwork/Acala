785e414279f650cde64e2a8c41c59c694af23d8e
Shaun Wang <spxwang@gmail.com>
2021-06-22 12:18:39 +1200

    Make Alice as the only collator for Karura dev. (#1114)

--- node/service/src/chain_spec/karura.rs
+++ node/service/src/chain_spec/karura.rs
@@ -199,11 +199,7 @@ pub fn karura_dev_config() -> Result<ChainSpec, String> {
 			karura_genesis(
 				wasm_binary,
 				// Initial PoA authorities
-				vec![
-					get_karura_authority_keys_from_seed("Alice"),
-					get_karura_authority_keys_from_seed("Bob"),
-					get_karura_authority_keys_from_seed("Charlie"),
-				],
+				vec![get_karura_authority_keys_from_seed("Alice")],
 				// Sudo account
 				get_account_id_from_seed::<sr25519::Public>("Alice"),
 				vec![
