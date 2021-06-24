9bea5a7205e68b0ed023d4a1665446928182f38e
zjb0807 <zjb0807@qq.com>
2021-06-25 07:46:03 +0800

    Update verify-bootnodes.sh

--- scripts/verify-bootnodes.sh
+++ scripts/verify-bootnodes.sh
@@ -3,11 +3,11 @@
 set -e
 
 if [ $1 = "karura" ]; then
-	IMAGE=acala/karura-node:1.0.1
+	IMAGE=acala/karura-node:latest
 	CHAIN_NAME=karura
 	GENESIS=./resources/karura-dist.json
 elif [ $1 = "acala" ]; then
-	IMAGE=acala/acala-node:1.0.1
+	IMAGE=acala/acala-node:latest
 	CHAIN_NAME=acala
 	GENESIS=
 else
@@ -38,22 +38,39 @@ do
 		   -- \
 		   --chain=dev \
 		> /dev/null
+	sleep 3s
 
-	sleep 8s
-	peer=$(curl -sS \
-         -H 'Content-Type: application/json' \
-         --data '{"id":1,"jsonrpc":"2.0","method":"system_peers"}' \
-         localhost:9933 |\
-		jq -r '.result')
+	echo ""
+	echo "Try to connect to $node"
+	TRY_TIMES=100
+	for ((i = 1; i <= $TRY_TIMES; i++))
+	do
+		peer=$(curl -sS \
+			--connect-timeout 5 -m 5 \
+			-H 'Content-Type: application/json' \
+			--data '{"id":1,"jsonrpc":"2.0","method":"system_peers"}' \
+			localhost:9933 |\
+			jq -r '.result')
 
-	peer_num=$(echo $peer | jq 'length')
-	peer_id=$(echo $peer | jq -r '.[0].peerId')
+		peer_num=$(echo $peer | jq 'length')
+		peer_id=$(echo $peer | jq -r '.[0].peerId')
 
-	if [ "$peer_num" != "1" ]; then
-		echo "Connect to $node failed. Node connect to peer number is: $peer_num"
-	elif [ $peer_id != ${node##*/} ]; then
-		echo "Connect to $node failed. The actual peer id: $peer_id"
-	fi
+		if [ "$peer_num" == "1" ] && [ $peer_id == ${node##*/} ]; then
+			echo "Connect to $node succeed."
+			break
+		fi 
+		sleep 3s
+
+		if [ $i == $TRY_TIMES ]; then
+			FAIL=true
+			echo "Connect to $node failed. Node connect to peer id: $peer_id, peer num: $peer_id"
+		fi
+	done
 
 	docker rm -f bootnotes > /dev/null
 done
+
+if [ -n $FAIL ]; then
+	echo "Bootnotes need to be checked."
+	exit 1
+fi
