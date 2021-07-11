c351b7ddf11674c5275e8b6c556f017ab5bdd4a8
zjb0807 <zjb0807@qq.com>
2021-07-11 15:45:12 +0800

    add session-manager module

---
+++ modules/session-manager/src/lib.rs
@@ -0,0 +1,239 @@
+// This file is part of Acala.
+
+// Copyright (C) 2020-2021 Acala Foundation.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+//! # Session Manager Module
+//!
+//! The module implement the `ShouldEndSession` and `EstimateNextSessionRotation`
+//! trait to handle the change of session time.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+#![allow(clippy::unused_unit)]
+
+use frame_support::{
+	pallet_prelude::*,
+	traits::{EstimateNextSessionRotation, ValidatorSet},
+};
+use frame_system::pallet_prelude::*;
+use pallet_session::ShouldEndSession;
+use sp_runtime::{
+	traits::{One, Saturating, Zero},
+	Permill,
+};
+use sp_staking::SessionIndex;
+
+mod mock;
+mod tests;
+
+pub use module::*;
+
+#[frame_support::pallet]
+pub mod module {
+	use super::*;
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+		/// A type for retrieving the validators supposed to be online in a session.
+		type ValidatorSet: ValidatorSet<Self::AccountId, ValidatorId = Self::AccountId>;
+	}
+
+	#[pallet::error]
+	pub enum Error<T> {
+		/// The session is invalid.
+		InvalidSession,
+		/// The duration is invalid.
+		InvalidDuration,
+		/// Failed to estimate next session.
+		EstimateNextSessionFailed,
+	}
+
+	#[pallet::event]
+	#[pallet::generate_deposit(fn deposit_event)]
+	pub enum Event<T: Config> {
+		/// Scheduled session duration. \[block_number, session_index, session_duration\]
+		ScheduledSessionDuration(T::BlockNumber, SessionIndex, T::BlockNumber),
+	}
+
+	/// The current session duration.
+	///
+	/// SessionDuration: SessionDuration
+	#[pallet::storage]
+	#[pallet::getter(fn session_duration)]
+	type SessionDuration<T: Config> = StorageValue<_, T::BlockNumber, ValueQuery>;
+
+	/// The current session duration offset.
+	///
+	/// DurationOffset: DurationOffset
+	#[pallet::storage]
+	#[pallet::getter(fn duration_offset)]
+	type DurationOffset<T: Config> = StorageValue<_, T::BlockNumber, ValueQuery>;
+
+	/// Mapping from block number to new session index and duration.
+	///
+	/// SessionDurationChanges: map BlockNumber => (SessionIndex, SessionDuration)
+	#[pallet::storage]
+	#[pallet::getter(fn session_duration_changes)]
+	pub type SessionDurationChanges<T: Config> =
+		StorageMap<_, Twox64Concat, T::BlockNumber, (SessionIndex, T::BlockNumber), ValueQuery>;
+
+	#[pallet::genesis_config]
+	pub struct GenesisConfig<T: Config> {
+		pub session_duration: T::BlockNumber,
+	}
+
+	impl<T: Config> Default for GenesisConfig<T> {
+		fn default() -> Self {
+			GenesisConfig {
+				session_duration: Default::default(),
+			}
+		}
+	}
+
+	#[pallet::genesis_build]
+	impl<T: Config> GenesisBuild<T> for GenesisConfig<T> {
+		fn build(&self) {
+			assert!(!self.session_duration.is_zero(), "SessionDuration can't be zero");
+			SessionDuration::<T>::put(self.session_duration);
+		}
+	}
+
+	#[pallet::pallet]
+	pub struct Pallet<T>(_);
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<T::BlockNumber> for Pallet<T> {
+		fn on_initialize(n: T::BlockNumber) -> Weight {
+			SessionDurationChanges::<T>::mutate_exists(n, |maybe_changes| {
+				if let Some((_, duration)) = maybe_changes.take() {
+					SessionDuration::<T>::put(duration);
+					DurationOffset::<T>::put(n);
+				}
+			});
+			10000
+		}
+	}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::weight(10000)]
+		pub fn schedule_session_duration(
+			origin: OriginFor<T>,
+			start_session: SessionIndex,
+			duration: T::BlockNumber,
+		) -> DispatchResultWithPostInfo {
+			ensure_root(origin)?;
+
+			let target_block_number = Self::do_schedule_session_duration(start_session, duration)?;
+
+			Self::deposit_event(Event::ScheduledSessionDuration(
+				target_block_number,
+				start_session,
+				duration,
+			));
+			Ok(().into())
+		}
+	}
+}
+
+impl<T: Config> Pallet<T> {
+	pub fn do_schedule_session_duration(
+		start_session: SessionIndex,
+		duration: T::BlockNumber,
+	) -> Result<T::BlockNumber, DispatchError> {
+		let block_number = <frame_system::Pallet<T>>::block_number();
+		let current_session = T::ValidatorSet::session_index();
+
+		ensure!(start_session > current_session, Error::<T>::InvalidSession);
+		ensure!(!duration.is_zero(), Error::<T>::InvalidDuration);
+
+		if duration == Self::session_duration() {
+			return Ok(block_number);
+		}
+
+		let next_session = Self::estimate_next_session_rotation(block_number)
+			.0
+			.ok_or(Error::<T>::EstimateNextSessionFailed)?;
+		let target_block_number = Into::<T::BlockNumber>::into(start_session - current_session - 1)
+			.saturating_mul(Self::session_duration())
+			.saturating_add(next_session);
+
+		SessionDurationChanges::<T>::insert(target_block_number, (start_session, duration));
+
+		Ok(target_block_number)
+	}
+}
+
+impl<T: Config> ShouldEndSession<T::BlockNumber> for Pallet<T> {
+	fn should_end_session(now: T::BlockNumber) -> bool {
+		let offset = Self::duration_offset();
+		now >= offset && ((now - offset) % Self::session_duration()).is_zero()
+	}
+}
+
+impl<T: Config> EstimateNextSessionRotation<T::BlockNumber> for Pallet<T> {
+	fn average_session_length() -> T::BlockNumber {
+		Self::session_duration()
+	}
+
+	fn estimate_current_session_progress(now: T::BlockNumber) -> (Option<Permill>, Weight) {
+		let offset = Self::duration_offset();
+		let period = Self::session_duration();
+
+		// NOTE: we add one since we assume that the current block has already elapsed,
+		// i.e. when evaluating the last block in the session the progress should be 100%
+		// (0% is never returned).
+		let progress = if now >= offset {
+			let current = (now - offset) % period + One::one();
+			Some(Permill::from_rational(current, period))
+		} else {
+			Some(Permill::from_rational(now + One::one(), offset))
+		};
+
+		// Weight note: `estimate_current_session_progress` has no storage reads and trivial
+		// computational overhead. There should be no risk to the chain having this weight value be
+		// zero for now. However, this value of zero was not properly calculated, and so it would be
+		// reasonable to come back here and properly calculate the weight of this function.
+		(progress, Zero::zero())
+	}
+
+	fn estimate_next_session_rotation(now: T::BlockNumber) -> (Option<T::BlockNumber>, Weight) {
+		let offset = Self::duration_offset();
+		let period = Self::session_duration();
+
+		let next_session = if now > offset {
+			let block_after_last_session = (now - offset) % period;
+			if block_after_last_session > Zero::zero() {
+				now.saturating_add(period.saturating_sub(block_after_last_session))
+			} else {
+				// this branch happens when the session is already rotated or will rotate in this
+				// block (depending on being called before or after `session::on_initialize`). Here,
+				// we assume the latter, namely that this is called after `session::on_initialize`,
+				// and thus we add period to it as well.
+				now + period
+			}
+		} else {
+			offset
+		};
+
+		// Weight note: `estimate_next_session_rotation` has no storage reads and trivial
+		// computational overhead. There should be no risk to the chain having this weight value be
+		// zero for now. However, this value of zero was not properly calculated, and so it would be
+		// reasonable to come back here and properly calculate the weight of this function.
+		(Some(next_session), Zero::zero())
+	}
+}
