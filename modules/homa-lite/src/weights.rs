60802b25ba7858b6c174fbac7138aac456de35fd
Roy Yang <roy@laminar.one>
2021-07-15 09:42:56 +1200

    Fixed a clippy warning/error that causes the build to fail on CI

--- modules/homa-lite/src/weights.rs
+++ modules/homa-lite/src/weights.rs
@@ -40,6 +40,7 @@
 #![cfg_attr(rustfmt, rustfmt_skip)]
 #![allow(unused_parens)]
 #![allow(unused_imports)]
+#![allow(clippy::unnecessary_cast)]
 
 use frame_support::{traits::Get, weights::{Weight, constants::RocksDbWeight}};
 use sp_std::marker::PhantomData;
