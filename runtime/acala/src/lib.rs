4816e76ccc3279c75af4f51fd378dfb69ac9f026
zjb0807 <zjb0807@qq.com>
2021-07-16 21:56:41 +0800

    Remove RewardsVaultAccountId (#1224)
    * remove RewardsVaultAccountId
    
    * update homa-lite version
    
    * add .codecov.yml
    
    * CI ignore README
    
    * fix benchmarks
    
    * add CARGO_INCREMENTAL

--- runtime/acala/src/lib.rs
+++ runtime/acala/src/lib.rs
@@ -1128,7 +1128,6 @@ impl module_incentives::Config for Runtime {
 	type Event = Event;
 	type RelaychainAccountId = AccountId;
 	type NativeRewardsSource = UnreleasedNativeVaultAccountId;
-	type RewardsVaultAccountId = ZeroAccountId;
 	type NativeCurrencyId = GetNativeCurrencyId;
 	type StableCurrencyId = GetStableCurrencyId;
 	type LiquidCurrencyId = GetLiquidCurrencyId;
--- runtime/mandala/src/lib.rs
+++ runtime/mandala/src/lib.rs
@@ -1150,7 +1150,6 @@ impl module_incentives::Config for Runtime {
 	type Event = Event;
 	type RelaychainAccountId = AccountId;
 	type NativeRewardsSource = UnreleasedNativeVaultAccountId;
-	type RewardsVaultAccountId = ZeroAccountId;
 	type NativeCurrencyId = GetNativeCurrencyId;
 	type StableCurrencyId = GetStableCurrencyId;
 	type LiquidCurrencyId = GetLiquidCurrencyId;
