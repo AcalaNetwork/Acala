e03bd5b3c6a6a12e627b38fddcd2fd6a5bad4e92
Shaun Wang <spxwang@gmail.com>
2021-08-13 11:31:33 +1200

    Homa-lite: use 'Any' network id for sovereign sub-account.

--- runtime/karura/src/lib.rs
+++ runtime/karura/src/lib.rs
@@ -1458,7 +1458,7 @@ pub fn create_x2_parachain_multilocation(index: u16) -> MultiLocation {
 	MultiLocation::X2(
 		Junction::Parent,
 		Junction::AccountId32 {
-			network: RelayNetwork::get(),
+			network: NetworkId::Any,
 			id: Utility::derivative_account_id(ParachainInfo::get().into_account(), index).into(),
 		},
 	)
