2fab35c9ab3fa3c841f12bc5b3f943fec9564555
Roy Yang <roy@laminar.one>
2021-09-02 17:19:46 +1200

    Fixed a benchmarking test

--- modules/homa-lite/src/benchmarking.rs
+++ modules/homa-lite/src/benchmarking.rs
@@ -41,7 +41,7 @@ benchmarks! {
 
 	set_total_staking_currency {}: _(RawOrigin::Root, 1_000_000_000_000)
 
-	adjust_total_staking_currency {}: _(RawOrigin::Root, 1_000_000_000_000)
+	adjust_total_staking_currency {}: _(RawOrigin::Root, AmountOf::<T>::default())
 
 	set_minting_cap {
 	}: _(RawOrigin::Root, 1_000_000_000_000_000_000)
