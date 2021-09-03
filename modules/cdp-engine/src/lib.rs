72ccd0c11c4d53fd0b4d1c09244e6d3295867a76
ferrell-code <automatedcharlesferrell@gmail.com>
2021-09-02 23:15:35 -0400

    Use frame-support library instead of orml-utilities for Iterators (#1390)
    * migrate to iterators in frame-support
    
    * use for loops instead
    
    * Revert "use for loops instead"
    
    This reverts commit 83aa194fc5c8264c39c9010450d9fb5408b92b05.
    
    * Revert "migrate to iterators in frame-support"
    
    This reverts commit 43fa10ee49769c3428a99de9266ee2ca6248f257.
    
    * keeps logic identical

--- modules/cdp-engine/src/lib.rs
+++ modules/cdp-engine/src/lib.rs
@@ -35,7 +35,7 @@ use frame_system::{
 };
 use loans::Position;
 use orml_traits::Change;
-use orml_utilities::{IterableStorageDoubleMapExtended, OffchainErr};
+use orml_utilities::OffchainErr;
 use primitives::{Amount, Balance, CurrencyId};
 use rand_chacha::{
 	rand_core::{RngCore, SeedableRng},
@@ -648,21 +648,25 @@ impl<T: Config> Pallet<T> {
 		// get the max iterationns config
 		let max_iterations = StorageValueRef::persistent(OFFCHAIN_WORKER_MAX_ITERATIONS)
 			.get::<u32>()
-			.unwrap_or(Some(DEFAULT_MAX_ITERATIONS));
+			.unwrap_or(Some(DEFAULT_MAX_ITERATIONS))
+			.ok_or(OffchainErr::OffchainStore)?;
 
 		let currency_id = collateral_currency_ids[collateral_position as usize];
 		let is_shutdown = T::EmergencyShutdown::is_shutdown();
-		let mut map_iterator = <loans::Positions<T> as IterableStorageDoubleMapExtended<_, _, _>>::iter_prefix(
-			currency_id,
-			max_iterations,
-			start_key.clone(),
-		);
+		let mut map_iterator =
+			<loans::Positions<T>>::iter_prefix_from(currency_id, start_key.clone().ok_or(OffchainErr::OffchainStore)?);
 
+		let mut finished = true;
 		let mut iteration_count = 0;
 		let iteration_start_time = sp_io::offchain::timestamp();
 
 		#[allow(clippy::while_let_on_iterator)]
 		while let Some((who, Position { collateral, debit })) = map_iterator.next() {
+			if iteration_count > max_iterations {
+				finished = false;
+				break;
+			}
+
 			if !is_shutdown
 				&& matches!(
 					Self::check_cdp_status(currency_id, collateral, debit),
@@ -695,7 +699,7 @@ impl<T: Config> Pallet<T> {
 
 		// if iteration for map storage finished, clear to be continue record
 		// otherwise, update to be continue record
-		if map_iterator.finished {
+		if finished {
 			let next_collateral_position =
 				if collateral_position < collateral_currency_ids.len().saturating_sub(1) as u32 {
 					collateral_position + 1
@@ -704,7 +708,7 @@ impl<T: Config> Pallet<T> {
 				};
 			to_be_continue.set(&(next_collateral_position, Option::<Vec<u8>>::None));
 		} else {
-			to_be_continue.set(&(collateral_position, Some(map_iterator.map_iterator.previous_key)));
+			to_be_continue.set(&(collateral_position, Some(map_iterator.last_raw_key())));
 		}
 
 		// Consume the guard but **do not** unlock the underlying lock.
