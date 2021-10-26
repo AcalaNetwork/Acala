9aa7b2c9e6443471be8a3e4257d3e0801ba23615
ferrell-code <automatedcharlesferrell@gmail.com>
2021-10-26 19:47:50 -0400

    orml branch

--- node/e2e-tests/test-runner/src/utils.rs
+++ node/e2e-tests/test-runner/src/utils.rs
@@ -64,7 +64,7 @@ pub fn default_config(tokio_handle: Handle, mut chain_spec: Box<dyn ChainSpec>) 
 	network_config.allow_non_globals_in_dht = true;
 
 	network_config
-		.listen_addresses
+		.listen_addresseas
 		.push(multiaddr::Protocol::Memory(0).into());
 
 	network_config.transport = TransportConfig::MemoryOnly;
@@ -116,7 +116,7 @@ pub fn default_config(tokio_handle: Handle, mut chain_spec: Box<dyn ChainSpec>) 
 		base_path: Some(base_path),
 		wasm_runtime_overrides: None,
 		informant_output_format,
-		ws_max_out_buffer_capacity: Default::default(),
+		ws_max_out_buffer_capacity: None,
 		keystore_remote: None,
 		keep_blocks: KeepBlocks::All,
 		state_pruning: Default::default(),
