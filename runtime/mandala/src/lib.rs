a2e2e25ee1460d4e670e7636afe77dd6a0eeb270
Xiliang Chen <xlchen1291@gmail.com>
2021-11-29 12:56:16 +1300

    make evm call and estimateResources public (#1649)
    * make evm call and estimateResources public
    
    * fix
    
    * bump
    
    * update bootnode
    
    * fix test

--- runtime/mandala/src/lib.rs
+++ runtime/mandala/src/lib.rs
@@ -138,7 +138,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("mandala"),
 	impl_name: create_runtime_str!("mandala"),
 	authoring_version: 1,
-	spec_version: 2002,
+	spec_version: 2004,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
