dda7edf23588d74ececfed7599f11633a19fa17e
Roy Yang <roy@laminar.one>
2021-12-01 22:56:46 +1300

    Updated a test that was fixed

--- runtime/integration-tests/src/relaychain/relay_chain.rs
+++ runtime/integration-tests/src/relaychain/relay_chain.rs
@@ -163,7 +163,7 @@ mod karura_tests {
 			// Only leftover XCM fee remains in the account
 			assert_eq!(
 				kusama_runtime::Balances::free_balance(&parachain_account.clone()),
-				9_400_000_000
+				9_626_666_690
 			);
 		});
 	}
