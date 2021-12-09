e410e9f819b040b1a432da4b2c8182ce57e8a647
ferrell-code <automatedcharlesferrell@gmail.com>
2021-12-08 19:16:36 -0500

    Revert "cargo run --release --color=never --bin=acala --features=runtime-benchmarks --features=with-mandala-runtime -- benchmark --chain=dev --steps=50 --repeat=20 --pallet=nutsfinance_stable_asset --extrinsic=* --execution=wasm --wasm-execution=compiled --heap-pages=4096 --template=./templates/runtime-weight-template.hbs --output=./runtime/mandala/src/weights/"
    This reverts commit 910614ce916222262fbe734824675f39d097dccb.

--- runtime/mandala/src/weights/nutsfinance_stable_asset.rs
+++ runtime/mandala/src/weights/nutsfinance_stable_asset.rs
@@ -20,21 +20,22 @@
 //!
 //! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 4.0.0-dev
 //! DATE: 2021-12-08, STEPS: `50`, REPEAT: 20, LOW RANGE: `[]`, HIGH RANGE: `[]`
-//! EXECUTION: Some(Wasm), WASM-EXECUTION: Compiled, CHAIN: Some("dev"), DB CACHE: 128
+//! EXECUTION: Some(Wasm), WASM-EXECUTION: Compiled, CHAIN: Some("mandala-latest"), DB CACHE: 128
 
 // Executed Command:
 // target/release/acala
 // benchmark
-// --chain=dev
+// --chain=mandala-latest
 // --steps=50
 // --repeat=20
-// --pallet=nutsfinance_stable_asset
+// --pallet=*
 // --extrinsic=*
 // --execution=wasm
 // --wasm-execution=compiled
 // --heap-pages=4096
 // --template=./templates/runtime-weight-template.hbs
 // --output=./runtime/mandala/src/weights/
+
 
 #![cfg_attr(rustfmt, rustfmt_skip)]
 #![allow(unused_parens)]
@@ -45,14 +46,49 @@ use sp_std::marker::PhantomData;
 
 /// Weight functions for nutsfinance_stable_asset.
 pub struct WeightInfo<T>(PhantomData<T>);
-impl<T: frame_system::Config> nutsfinance_stable_asset::WeightInfo for WeightInfo<T> {	fn create_pool() -> Weight {
-		(41_988_000 as Weight)			.saturating_add(T::DbWeight::get().reads(3 as Weight))			.saturating_add(T::DbWeight::get().writes(3 as Weight))	}	fn modify_a() -> Weight {
-		(30_637_000 as Weight)			.saturating_add(T::DbWeight::get().reads(1 as Weight))			.saturating_add(T::DbWeight::get().writes(1 as Weight))	}	fn mint(u: u32, ) -> Weight {
-		(142_503_000 as Weight)			// Standard Error: 305_000
-			.saturating_add((39_256_000 as Weight).saturating_mul(u as Weight))			.saturating_add(T::DbWeight::get().reads(8 as Weight))			.saturating_add(T::DbWeight::get().reads((2 as Weight).saturating_mul(u as Weight)))			.saturating_add(T::DbWeight::get().writes(6 as Weight))			.saturating_add(T::DbWeight::get().writes((2 as Weight).saturating_mul(u as Weight)))	}	fn swap() -> Weight {
-		(145_265_000 as Weight)			.saturating_add(T::DbWeight::get().reads(9 as Weight))			.saturating_add(T::DbWeight::get().writes(7 as Weight))	}	fn redeem_proportion(u: u32, ) -> Weight {
-		(102_933_000 as Weight)			// Standard Error: 1_556_000
-			.saturating_add((36_063_000 as Weight).saturating_mul(u as Weight))			.saturating_add(T::DbWeight::get().reads(6 as Weight))			.saturating_add(T::DbWeight::get().reads((2 as Weight).saturating_mul(u as Weight)))			.saturating_add(T::DbWeight::get().writes(4 as Weight))			.saturating_add(T::DbWeight::get().writes((2 as Weight).saturating_mul(u as Weight)))	}	fn redeem_single() -> Weight {
-		(161_122_000 as Weight)			.saturating_add(T::DbWeight::get().reads(9 as Weight))			.saturating_add(T::DbWeight::get().writes(6 as Weight))	}	fn redeem_multi(u: u32, ) -> Weight {
-		(100_061_000 as Weight)			// Standard Error: 100_000
-			.saturating_add((29_231_000 as Weight).saturating_mul(u as Weight))			.saturating_add(T::DbWeight::get().reads(6 as Weight))			.saturating_add(T::DbWeight::get().reads((2 as Weight).saturating_mul(u as Weight)))			.saturating_add(T::DbWeight::get().writes(4 as Weight))			.saturating_add(T::DbWeight::get().writes((2 as Weight).saturating_mul(u as Weight)))	}}
+impl<T: frame_system::Config> nutsfinance_stable_asset::weights::WeightInfo for WeightInfo<T> {
+	fn create_pool() -> Weight {
+		(30_520_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(3 as Weight))
+			.saturating_add(T::DbWeight::get().writes(3 as Weight))
+	}
+	fn modify_a() -> Weight {
+		(22_908_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(1 as Weight))
+			.saturating_add(T::DbWeight::get().writes(1 as Weight))
+	}
+	fn mint(u: u32) -> Weight {
+		(86_058_000 as Weight) // Standard Error: 384_000
+			.saturating_add((33_480_000 as Weight).saturating_mul(u as Weight))
+			.saturating_add(T::DbWeight::get().reads(8 as Weight))
+			.saturating_add(T::DbWeight::get().reads((2 as Weight).saturating_mul(u as Weight)))
+			.saturating_add(T::DbWeight::get().writes(6 as Weight))
+			.saturating_add(T::DbWeight::get().writes((2 as Weight).saturating_mul(u as Weight)))
+	}
+	fn swap() -> Weight {
+		(109_646_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(9 as Weight))
+			.saturating_add(T::DbWeight::get().writes(7 as Weight))
+	}
+	fn redeem_proportion(u: u32) -> Weight {
+		(90_887_000 as Weight) // Standard Error: 51_000
+			.saturating_add((21_808_000 as Weight).saturating_mul(u as Weight))
+			.saturating_add(T::DbWeight::get().reads(6 as Weight))
+			.saturating_add(T::DbWeight::get().reads((2 as Weight).saturating_mul(u as Weight)))
+			.saturating_add(T::DbWeight::get().writes(4 as Weight))
+			.saturating_add(T::DbWeight::get().writes((2 as Weight).saturating_mul(u as Weight)))
+	}
+	fn redeem_single() -> Weight {
+		(122_123_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(9 as Weight))
+			.saturating_add(T::DbWeight::get().writes(6 as Weight))
+	}
+	fn redeem_multi(u: u32) -> Weight {
+		(76_186_000 as Weight) // Standard Error: 87_000
+			.saturating_add((21_640_000 as Weight).saturating_mul(u as Weight))
+			.saturating_add(T::DbWeight::get().reads(6 as Weight))
+			.saturating_add(T::DbWeight::get().reads((2 as Weight).saturating_mul(u as Weight)))
+			.saturating_add(T::DbWeight::get().writes(4 as Weight))
+			.saturating_add(T::DbWeight::get().writes((2 as Weight).saturating_mul(u as Weight)))
+	}
+}
