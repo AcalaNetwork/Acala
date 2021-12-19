a0af4f3f4ccba22f326fa4d7d320e1c0060c721c
zjb0807 <zjb0807@qq.com>
2021-12-19 17:33:31 +0800

    remove parachain-id and update launch

--- node/cli/src/cli.rs
+++ node/cli/src/cli.rs
@@ -93,10 +93,6 @@ pub struct ExportGenesisStateCommand {
 	#[structopt(parse(from_os_str))]
 	pub output: Option<PathBuf>,
 
-	/// Id of the parachain this state is for.
-	#[structopt(long, default_value = "2000")]
-	pub parachain_id: u32,
-
 	/// Write output in binary. Default is to write in hex.
 	#[structopt(short, long)]
 	pub raw: bool,
