c67b6f43ffe803f135caf05ed8b424488d99e666
Bryan Chen <xlchen1291@gmail.com>
2022-01-14 14:26:12 +1300

    bump version

--- runtime/mandala/src/lib.rs
+++ runtime/mandala/src/lib.rs
@@ -139,7 +139,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("mandala"),
 	impl_name: create_runtime_str!("mandala"),
 	authoring_version: 1,
-	spec_version: 2018,
+	spec_version: 2020,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
