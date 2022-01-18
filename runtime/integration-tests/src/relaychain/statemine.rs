06c053a1c259a3d7ebe5b2a3ed8d3b1430a8a222
Roy Yang <roy@laminar.one>
2022-01-18 13:28:41 +1300

    WIP: Transfer assets from westmint /statemine to karura

--- runtime/integration-tests/src/relaychain/statemine.rs
+++ runtime/integration-tests/src/relaychain/statemine.rs
@@ -16,54 +16,88 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! Cross-chain transfer tests within Kusama network.
+//! Tests parachain to parachain xcm communication between Statemine and Karura.
 
-use crate::relaychain::kusama_test_net::*;
-use crate::setup::*;
+#[cfg(feature = "with-karura-runtime")]
+mod statemine_tests {
+	use crate::relaychain::kusama_test_net::*;
+	use crate::setup::*;
 
-use frame_support::assert_ok;
-use polkadot_parachain::primitives::Sibling;
-use xcm::VersionedMultiLocation;
-use xcm_builder::AccountId32Aliases;
-use xcm_emulator::TestExt;
-use xcm_executor::traits::Convert;
+	use frame_support::{assert_ok, traits::Hooks};
+	use polkadot_parachain::primitives::Sibling;
+	use xcm::{
+		v1::{Junction, Junctions, MultiAsset, MultiAssets, MultiLocation},
+		VersionedMultiAssets, VersionedMultiLocation,
+	};
+	use xcm_builder::AccountId32Aliases;
+	use xcm_emulator::TestExt;
+	use xcm_executor::traits::Convert;
 
-#[test]
-fn statemine() {
-	env_logger::init();
-	Statemine::execute_with(|| {
-		use westmint_runtime::*;
+	#[test]
+	fn can_transfer_custom_asset_into_karura() {
+		env_logger::init();
+		Statemine::execute_with(|| {
+			use westmint_runtime::*;
 
-		let origin = Origin::signed(ALICE.into());
+			let origin = Origin::signed(ALICE.into());
+			Balances::make_free_balance_be(&ALICE.into(), 10 * dollar(KSM));
 
-		Balances::make_free_balance_be(&ALICE.into(), 10 * dollar(KSM));
+			// need to have some KSM to be able to receive user assets
+			Balances::make_free_balance_be(&Sibling::from(2000).into_account(), 10 * dollar(KSM));
 
-		// need to have some KSM to be able to receive user assets
-		Balances::make_free_balance_be(&Sibling::from(2000).into_account(), 10 * dollar(KSM));
+			assert_ok!(Assets::create(origin.clone(), 0, MultiAddress::Id(ALICE.into()), 10,));
+			assert_ok!(Assets::mint(origin.clone(), 0, MultiAddress::Id(ALICE.into()), 1000));
 
-		assert_ok!(Assets::create(origin.clone(), 0, MultiAddress::Id(ALICE.into()), 10,));
+			let para_acc: AccountId = Sibling::from(2000).into_account();
 
-		assert_ok!(Assets::mint(origin.clone(), 0, MultiAddress::Id(ALICE.into()), 1000));
+			// KSM is used to pay for xcm execution
+			let multi_asset: VersionedMultiAssets = VersionedMultiAssets::V1(
+				vec![
+					MultiAsset {
+						id: Concrete(MultiLocation::here()),
+						fun: Fungibility::Fungible(dollar(KSM)),
+					},
+					MultiAsset {
+						id: Concrete(MultiLocation {
+							parents: 0,
+							interior: Junctions::X1(Junction::GeneralIndex(0)),
+						}),
+						fun: Fungibility::Fungible(100),
+					},
+				]
+				.into(),
+			);
+			// !todo : Figure out how to represent GeneralIndex(0) in MultiAsset so the transaction can go
+			// through
 
-		System::reset_events();
+			assert_ok!(PolkadotXcm::reserve_transfer_assets(
+				origin.clone(),
+				Box::new(MultiLocation::new(1, X1(Parachain(2000))).into()),
+				Box::new(
+					Junction::AccountId32 {
+						id: BOB,
+						network: NetworkId::Any
+					}
+					.into()
+					.into()
+				),
+				Box::new(multi_asset),
+				0
+			));
+			assert_eq!(Balances::free_balance(&ALICE.into()), 9 * dollar(KSM));
+			assert_eq!(Assets::balance(0, &para_acc), 100);
+		});
 
-		let para_acc: AccountId = Sibling::from(2000).into_account();
-		println!("{:?}", para_acc);
+		// Rerun the Statemine::execute to actually send the egress message via XCM
+		Statemine::execute_with(|| {
+			let para_acc: AccountId = Sibling::from(2000).into_account();
+			assert_eq!(westmint_runtime::Assets::balance(0, &para_acc), 100);
+			println!("Westmint: {:?}", westmint_runtime::System::events());
+		});
 
-		assert_ok!(PolkadotXcm::reserve_transfer_assets(
-			origin.clone(),
-			Box::new(MultiLocation::new(1, X1(Parachain(2000),)).into()),
-			Box::new(
-				Junction::AccountId32 {
-					id: BOB,
-					network: NetworkId::Any
-				}
-				.into()
-				.into()
-			),
-			Box::new((GeneralIndex(0), 100).into()),
-			0
-		));
-		println!("{:?}", System::events());
-	});
+		Karura::execute_with(|| {
+			println!("Karura: {:?}", karura_runtime::System::events());
+			// assert_eq!(Tokens::free_balance(KSM, &AccountId::from(BOB)), 0);
+		});
+	}
 }
