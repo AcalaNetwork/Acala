fa4750f7ed823c378f93e58e2e9c307a33c294f7
Shaun Wang <spxwang@gmail.com>
2022-01-18 14:22:28 +1300

    clippy

--- rpc/evm-tracing/src/formatters/call_tracer.rs
+++ rpc/evm-tracing/src/formatters/call_tracer.rs
@@ -53,9 +53,9 @@ impl super::ResponseFormatter for Formatter {
 					let gas_used = it.gas_used;
 					let inner = it.inner.clone();
 					Some(Call::CallTracer(CallTracerCall {
-						from: from,
-						gas: gas,
-						gas_used: gas_used,
+						from,
+						gas,
+						gas_used,
 						trace_address: Some(trace_address.clone()),
 						inner: match inner.clone() {
 							BlockscoutCallInner::Call {
@@ -94,7 +94,7 @@ impl super::ResponseFormatter for Formatter {
 									} => Some(created_contract_code),
 									CreateResult::Error { .. } => None,
 								},
-								value: value,
+								value,
 								call_type: "CREATE".as_bytes().to_vec(),
 							},
 							BlockscoutCallInner::SelfDestruct { balance, to } => CallTracerInner::SelfDestruct {
