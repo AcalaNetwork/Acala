03ba7150ff79cf937f4702b9a4f0b19b7d93f164
zqh <zqhxuyuan@gmail.com>
2022-01-19 09:45:37 +0800

    try-runtime works only on dev

--- node/cli/src/command.rs
+++ node/cli/src/command.rs
@@ -206,6 +206,17 @@ fn set_default_ss58_version(spec: &Box<dyn service::ChainSpec>) {
 	sp_core::crypto::set_default_ss58_version(ss58_version.into());
 }
 
+const DEV_ONLY_ERROR_PATTERN: &'static str =
+	"can only use subcommand with --chain [karura-dev, acala-dev, pc-dev, dev], got ";
+
+fn ensure_dev(spec: &Box<dyn service::ChainSpec>) -> std::result::Result<(), String> {
+	if spec.is_dev() {
+		Ok(())
+	} else {
+		Err(format!("{}{}", DEV_ONLY_ERROR_PATTERN, spec.id()))
+	}
+}
+
 fn extract_genesis_wasm(chain_spec: &Box<dyn service::ChainSpec>) -> Result<Vec<u8>> {
 	let mut storage = chain_spec.build_storage()?;
 
@@ -425,8 +436,9 @@ pub fn run() -> sc_cli::Result<()> {
 		Some(Subcommand::TryRuntime(cmd)) => {
 			let runner = cli.create_runner(cmd)?;
 			let chain_spec = &runner.config().chain_spec;
-
 			set_default_ss58_version(chain_spec);
+
+			ensure_dev(chain_spec).map_err(|err| format!("try-runtime error: {}", err))?;
 
 			with_runtime_or_err!(chain_spec, {
 				return runner.async_run(|config| {
