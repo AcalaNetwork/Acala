b138d509f01555c0724d09451ded5128199b1a1c
Bryan Chen <xlchen1291@gmail.com>
2022-03-21 14:48:53 +1300

    bump version

--- runtime/karura/src/lib.rs
+++ runtime/karura/src/lib.rs
@@ -123,7 +123,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("karura"),
 	impl_name: create_runtime_str!("karura"),
 	authoring_version: 1,
-	spec_version: 2040,
+	spec_version: 2041,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
