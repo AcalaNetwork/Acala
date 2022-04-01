3d1599110c8260752be288241e2bf8e63143cd6c
zjb0807 <zjb0807@qq.com>
2022-04-01 13:05:14 +0800

    fix srtool (#2013)

--- runtime/common/src/precompile/mod.rs
+++ runtime/common/src/precompile/mod.rs
@@ -77,7 +77,8 @@ pub const SCHEDULER: H160 = H160(hex!("0000000000000000000000000000000000000404"
 pub const DEX: H160 = H160(hex!("0000000000000000000000000000000000000405"));
 
 pub fn target_gas_limit(target_gas: Option<u64>) -> Option<u64> {
-	target_gas.map(|x| x.saturating_div(10).saturating_mul(9)) // 90%
+	// srtool support rust 1.57.0
+	target_gas.map(|x| x.checked_div(10).expect("divisor is non-zero; qed").saturating_mul(9)) // 90%
 }
 
 pub struct AllPrecompiles<R> {
