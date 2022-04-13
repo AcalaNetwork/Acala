d9a42249ee420c9ad82c7fd34aca84c2ad042e8c
Bryan Chen <xlchen1291@gmail.com>
2022-04-13 15:00:46 +1200

    fix build script

--- scripts/docker-hub-publish-acala-dev.sh
+++ scripts/docker-hub-publish-acala-dev.sh
@@ -6,5 +6,5 @@ VERSION=$(git rev-parse --short HEAD)
 NODE_NAME=acala/acala-node
 BUILD_ARGS="build-acala-internal-release"
 
-docker build -f scripts/Dockerfile . -t $NODE_NAME:$VERSION --build-arg GIT_COMMIT=${VERSION} --build-arg BUILD_ARGS="$BUILD_ARGS"
+docker build -f scripts/Dockerfile . -t $NODE_NAME:$VERSION --build-arg GIT_COMMIT=${VERSION} --build-arg BUILD_ARGS="$BUILD_ARGS" --build-arg PROFILE=release
 docker push $NODE_NAME:$VERSION
