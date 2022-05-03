515d6834b0d2be4e3c2c101acdaaafd0a8b2582f
Bryan Chen <xlchen1291@gmail.com>
2022-05-03 16:49:44 +1200

    reduce gas limit for erc20 operations

--- modules/support/src/evm.rs
+++ modules/support/src/evm.rs
@@ -224,3 +224,27 @@ impl Erc20InfoMapping for () {
 		None
 	}
 }
+
+pub mod limits {
+	pub struct Limit {
+		pub gas: u64,
+		pub storage: u32,
+	}
+
+	impl Limit {
+		pub const fn new(gas: u64, storage: u32) -> Self {
+			Self { gas, storage }
+		}
+	}
+
+	pub mod erc20 {
+		use super::*;
+
+		pub const NAME: Limit = Limit::new(10_000, 0);
+		pub const SYMBOL: Limit = Limit::new(10_000, 0);
+		pub const DECIMALS: Limit = Limit::new(10_000, 0);
+		pub const TOTAL_SUPPLY: Limit = Limit::new(10_000, 0);
+		pub const BALANCE_OF: Limit = Limit::new(10_000, 0);
+		pub const TRANSFER: Limit = Limit::new(200_000, 960);
+	}
+}
