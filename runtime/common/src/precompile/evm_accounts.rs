4d8d63a170b228e456afe870be80ba491292d44d
zjb0807 <zjb0807@qq.com>
2022-05-23 09:35:32 +0800

    Add claimDefaultEvmAddress (#2149)
    * add claimDefaultEvmAddress

--- runtime/common/src/precompile/evm_accounts.rs
+++ runtime/common/src/precompile/evm_accounts.rs
@@ -27,6 +27,7 @@ use module_evm::{
 	runner::state::{PrecompileFailure, PrecompileOutput, PrecompileResult},
 	Context, ExitError, ExitRevert, ExitSucceed,
 };
+use module_evm_accounts::WeightInfo;
 use module_support::EVMAccountsManager;
 use num_enum::{IntoPrimitive, TryFromPrimitive};
 use sp_runtime::{traits::Convert, AccountId32, RuntimeDebug};
@@ -47,6 +48,7 @@ pub struct EVMAccountsPrecompile<R>(PhantomData<R>);
 pub enum Action {
 	GetAccountId = "getAccountId(address)",
 	GetEvmAddress = "getEvmAddress(bytes32)",
+	ClaimDefaultEvmAddress = "claimDefaultEvmAddress(bytes32)",
 }
 
 impl<Runtime> Precompile for EVMAccountsPrecompile<Runtime>
@@ -109,6 +111,30 @@ where
 					logs: Default::default(),
 				})
 			}
+			Action::ClaimDefaultEvmAddress => {
+				// bytes32
+				let input_data = input.bytes_at(1, 32)?;
+
+				let mut buf = [0u8; 32];
+				buf.copy_from_slice(&input_data[..]);
+				let account_id: Runtime::AccountId = AccountId32::from(buf).into();
+
+				let address =
+					module_evm_accounts::Pallet::<Runtime>::claim_default_evm_address(&account_id).map_err(|e| {
+						PrecompileFailure::Revert {
+							exit_status: ExitRevert::Reverted,
+							output: Into::<&str>::into(e).as_bytes().to_vec(),
+							cost: target_gas_limit(target_gas).unwrap_or_default(),
+						}
+					})?;
+
+				Ok(PrecompileOutput {
+					exit_status: ExitSucceed::Returned,
+					cost: gas_cost,
+					output: Output::default().encode_address(&address),
+					logs: Default::default(),
+				})
+			}
 		}
 	}
 }
@@ -134,6 +160,12 @@ where
 				// EVMAccounts::EvmAddresses (r: 1)
 				WeightToGas::convert(<Runtime as frame_system::Config>::DbWeight::get().reads(1))
 			}
+			Action::ClaimDefaultEvmAddress => {
+				// claim_default_account weight
+				let weight = <Runtime as module_evm_accounts::Config>::WeightInfo::claim_default_account();
+
+				WeightToGas::convert(weight)
+			}
 		};
 		Ok(Self::BASE_COST.saturating_add(cost))
 	}
@@ -143,9 +175,12 @@ where
 mod tests {
 	use super::*;
 
-	use crate::precompile::mock::{alice_evm_addr, new_test_ext, Test};
+	use crate::precompile::mock::{alice_evm_addr, new_test_ext, EvmAddress, Test, ALICE};
+	use codec::Encode;
 	use frame_support::assert_noop;
 	use hex_literal::hex;
+	use sp_core::blake2_256;
+	use std::str::FromStr;
 
 	type EVMAccountsPrecompile = crate::precompile::EVMAccountsPrecompile<Test>;
 
@@ -214,6 +249,50 @@ mod tests {
 					exit_status: ExitRevert::Reverted,
 					output: "EvmAddress mapping not found".into(),
 					cost: target_gas_limit(Some(10_000)).unwrap(),
+				}
+			);
+		});
+	}
+
+	#[test]
+	fn claim_default_evm_address_works() {
+		new_test_ext().execute_with(|| {
+			let context = Context {
+				address: Default::default(),
+				caller: alice_evm_addr(),
+				apparent_value: Default::default(),
+			};
+
+			// claimDefaultEvmAddress(bytes32) -> 0xbe4327a6
+			// normal account_id: ALICE
+			let input = hex! {"
+				be4327a6
+				0101010101010101010101010101010101010101010101010101010101010101
+			"};
+
+			let payload = (b"evm:", ALICE);
+			let default_address = EvmAddress::from_slice(&payload.using_encoded(blake2_256)[0..20]);
+			assert_eq!(
+				default_address,
+				EvmAddress::from_str("0x8f2703bbe0abeaf09b384374959ffac5f7d0d69f").unwrap()
+			);
+
+			// expect output is evm address
+			let expected_output = hex! {"
+				000000000000000000000000 8f2703bbe0abeaf09b384374959ffac5f7d0d69f
+			"};
+
+			let resp = EVMAccountsPrecompile::execute(&input, None, &context, false).unwrap();
+			assert_eq!(resp.exit_status, ExitSucceed::Returned);
+			assert_eq!(resp.output, expected_output.to_vec());
+
+			// call again, the evm address already mapped
+			assert_noop!(
+				EVMAccountsPrecompile::execute(&input, Some(100_000), &context, false),
+				PrecompileFailure::Revert {
+					exit_status: ExitRevert::Reverted,
+					output: "AccountIdHasMapped".into(),
+					cost: target_gas_limit(Some(100_000)).unwrap(),
 				}
 			);
 		});
