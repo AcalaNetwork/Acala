df5604570f5b08cfdfeb5040737d808a1bdf140d
Bryan Chen <xlchen1291@gmail.com>
2022-05-25 11:50:27 +1200

    2.6.4

--- runtime/karura/src/lib.rs
+++ runtime/karura/src/lib.rs
@@ -130,7 +130,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("karura"),
 	impl_name: create_runtime_str!("karura"),
 	authoring_version: 1,
-	spec_version: 2063,
+	spec_version: 2064,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
