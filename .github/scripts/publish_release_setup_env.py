2a0d5b7c95804c7ca49976d5c9c7b6d8a961fdfe
Ermal Kaleci <ermalkaleci@gmail.com>
2022-08-11 11:23:10 +0200

    fix script

--- .github/scripts/publish_release_setup_env.py
+++ .github/scripts/publish_release_setup_env.py
@@ -2,7 +2,7 @@ import os
 import inspect
 
 chain, version = inspect.get_chain_and_version(os.getenv("GITHUB_REF"))
-previous_version = inspect.get_previus_version(chain)
+previous_version = inspect.get_previous_version(chain)
 
 is_patch = previous_version.split(".")[1] == version.split(".")[1]
 scope = "runtime" if is_patch else "full"
