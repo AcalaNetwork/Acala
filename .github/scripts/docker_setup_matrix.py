c20d32033ceb73f5f0017242772172a4400674d3
Ermal Kaleci <ermalkaleci@gmail.com>
2022-08-11 22:08:34 +0200

    docker image with version

--- .github/scripts/docker_setup_matrix.py
+++ .github/scripts/docker_setup_matrix.py
@@ -4,7 +4,8 @@ import inspect
 branch = sys.argv[1] if len(sys.argv) > 1 else ''
 
 if branch.__contains__("release-"):
-    chain, version = inspect.get_chain_and_version(branch)
-    print("::set-output name=matrix::{\"network\": [\"" + chain + "\"]}")
+	chain, version = inspect.get_chain_and_version(branch)
+	print("::set-output name=matrix::{\"network\": [\"" + chain + "\"]}")
+	print("::set-output name=version::" + version + "")
 else:
     print("::set-output name=matrix::{\"network\": [\"mandala\", \"karura\", \"acala\"]}")
