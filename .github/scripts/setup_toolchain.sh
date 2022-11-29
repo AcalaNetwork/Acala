dc01adf7306a061b7b7176c91a3e7b2679022fc5
Ermal Kaleci <ermalkaleci@gmail.com>
2022-11-29 16:28:55 +0100

    use make toolchain

--- .github/scripts/setup_toolchain.sh
+++ .github/scripts/setup_toolchain.sh
@@ -6,8 +6,6 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 
 source "$HOME/.cargo/env"
 
-rustup default nightly-2022-08-05
-
-rustup target add wasm32-unknown-unknown --toolchain nightly-2022-08-05
+make toolchain
 
 echo "$HOME/.cargo/bin" >> $GITHUB_PATH
