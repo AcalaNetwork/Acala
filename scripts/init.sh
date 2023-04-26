54907ca2e284150459851f72a0dd2212c9ca4519
zjb0807 <zjb0807@qq.com>
2023-04-26 17:00:52 +0800

    Upgrade rust-1.69 and srtool

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "*** Initializing WASM build environment"
 
-rustup default nightly-2022-10-30
+rustup default nightly-2023-03-04
 
-rustup target add wasm32-unknown-unknown --toolchain nightly-2022-10-30
+rustup target add wasm32-unknown-unknown --toolchain nightly-2023-03-04
