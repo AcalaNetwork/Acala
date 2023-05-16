2add5357b394f080d4d4322ad90e039844c577ed
wangjj9219 <183318287@qq.com>
2023-05-16 09:56:38 +0800

    fix tests

--- runtime/common/src/precompile/schedule.rs
+++ runtime/common/src/precompile/schedule.rs
@@ -489,7 +489,7 @@ mod tests {
 			run_to_block(5);
 			#[cfg(not(feature = "with-ethereum-compatibility"))]
 			{
-				assert_eq!(Balances::free_balance(from_account.clone()), 999999935591);
+				assert_eq!(Balances::free_balance(from_account.clone()), 999999931325);
 				assert_eq!(Balances::reserved_balance(from_account), 0);
 				assert_eq!(Balances::free_balance(to_account), 1000000001000);
 			}
