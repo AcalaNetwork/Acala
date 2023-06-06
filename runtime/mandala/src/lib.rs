b6b17257a91ee8e771ebf1bebf8e0f290d3426d1
Bryan Chen <xlchen1291@gmail.com>
2023-06-06 22:57:55 +1200

    use the new convert trait

--- runtime/mandala/src/lib.rs
+++ runtime/mandala/src/lib.rs
@@ -82,7 +82,7 @@ use sp_runtime::{
 		StaticLookup, Verify,
 	},
 	transaction_validity::{TransactionSource, TransactionValidity},
-	ApplyExtrinsicResult, DispatchResult, FixedPointNumber,
+	ApplyExtrinsicResult, ArithmeticError, DispatchResult, FixedPointNumber,
 };
 use sp_std::prelude::*;
 
@@ -1692,21 +1692,23 @@ pub struct ConvertBalanceHoma;
 impl orml_tokens::ConvertBalance<Balance, Balance> for ConvertBalanceHoma {
 	type AssetId = CurrencyId;
 
-	fn convert_balance(balance: Balance, asset_id: CurrencyId) -> Balance {
-		match asset_id {
-			CurrencyId::Token(TokenSymbol::LDOT) => Homa::get_exchange_rate().saturating_mul_int(balance),
+	fn convert_balance(balance: Balance, asset_id: CurrencyId) -> Result<Balance, ArithmeticError> {
+		Ok(match asset_id {
+			CurrencyId::Token(TokenSymbol::LDOT) => Homa::get_exchange_rate()
+				.checked_mul_int(balance)
+				.ok_or(ArithmeticError::Overflow)?,
 			_ => balance,
-		}
+		})
 	}
 
-	fn convert_balance_back(balance: Balance, asset_id: CurrencyId) -> Balance {
-		match asset_id {
+	fn convert_balance_back(balance: Balance, asset_id: CurrencyId) -> Result<Balance, ArithmeticError> {
+		Ok(match asset_id {
 			CurrencyId::Token(TokenSymbol::LDOT) => Homa::get_exchange_rate()
 				.reciprocal()
 				.and_then(|x| x.checked_mul_int(balance))
-				.unwrap_or_else(Bounded::max_value),
+				.ok_or(ArithmeticError::Overflow)?,
 			_ => balance,
-		}
+		})
 	}
 }
 
