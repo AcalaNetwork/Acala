0c1a872970066f9d0509e584a1f84866c46b6cfa
Xiliang Chen <xlchen1291@gmail.com>
2023-07-05 12:04:46 +1200

    2190 (#2567)
    * 2190
    
    * use nightly fmt

--- runtime/karura/src/lib.rs
+++ runtime/karura/src/lib.rs
@@ -129,7 +129,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("karura"),
 	impl_name: create_runtime_str!("karura"),
 	authoring_version: 1,
-	spec_version: 2180,
+	spec_version: 2190,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
