d75b21c1bf5e258148d787014cf81ac5f431e738
Bryan Chen <xlchen1291@gmail.com>
2023-07-24 11:31:51 +1200

    update deps

--- modules/evm/src/precompiles/ripemd.rs
+++ modules/evm/src/precompiles/ripemd.rs
@@ -31,7 +31,7 @@ impl LinearCostPrecompile for Ripemd160 {
 
 	fn execute(input: &[u8], _cost: u64) -> core::result::Result<(ExitSucceed, Vec<u8>), PrecompileFailure> {
 		let mut ret = [0u8; 32];
-		ret[12..32].copy_from_slice(&ripemd160::Ripemd160::digest(input));
+		ret[12..32].copy_from_slice(&ripemd::Ripemd160::digest(input));
 		Ok((ExitSucceed::Returned, ret.to_vec()))
 	}
 }
