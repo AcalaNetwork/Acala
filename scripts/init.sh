cab5cd8a8f234a0931bec4a854e805fdb896ffc6
zjb0807 <zjb0807@qq.com>
2023-11-08 05:13:35 +0800

    Remove rustup (#2642)
    * Remove rustup
    
    * fix CI

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "*** Initializing WASM build environment"
 
-rustup default nightly-2023-04-15
+rustup component add rustfmt --toolchain nightly
 
-rustup target add wasm32-unknown-unknown --toolchain nightly-2023-04-15
+rustup show
