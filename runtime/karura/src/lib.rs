1c3725200247666af4af23e57c6aa8479ccc527c
Bryan Chen <xlchen1291@gmail.com>
2023-12-01 12:44:38 +1300

    2.23.0

--- runtime/karura/src/lib.rs
+++ runtime/karura/src/lib.rs
@@ -133,7 +133,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("karura"),
 	impl_name: create_runtime_str!("karura"),
 	authoring_version: 1,
-	spec_version: 2220,
+	spec_version: 2230,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
