3679d07842675bd15736853d1569a3b60e212b8d
Bryan Chen <xlchen1291@gmail.com>
2023-12-15 13:07:30 +1300

    adjust track config

--- runtime/acala/src/governance/tracks.rs
+++ runtime/acala/src/governance/tracks.rs
@@ -38,11 +38,11 @@ static ref TRACKS_DATA: [(u16, pallet_referenda::TrackInfo<Balance, BlockNumber>
 			name: "root",
 			// A limit for the number of referenda on this track that can be being decided at once.
 			// For Root origin this should generally be just one.
-			max_deciding: 5,
+			max_deciding: 1,
 			// Amount that must be placed on deposit before a decision can be made.
-			decision_deposit: 20 * 1000 * dollar(ACA),
+			decision_deposit: 20_000 * dollar(ACA),
 			// Amount of time this must be submitted for before a decision can be made.
-			prepare_period: DAYS,
+			prepare_period: 2 * HOURS,
 			// Amount of time that a decision may take to be approved prior to cancellation.
 			decision_period: 14 * DAYS,
 			// Amount of time that the approval criteria must hold before it can be approved.
@@ -54,21 +54,21 @@ static ref TRACKS_DATA: [(u16, pallet_referenda::TrackInfo<Balance, BlockNumber>
 			min_approval: Curve::make_reciprocal(4, 14, percent(80), percent(50), percent(100)),
 			// Minimum pre-conviction aye-votes ("support") as percentage of overall population that
 			// is needed for approval as a function of time into decision period.
-			min_support: Curve::make_linear(14, 14, permill(5), percent(25)),
+			min_support: Curve::make_linear(14, 14, permill(1), percent(40)),
 		},
 	),
 	(
 		1,
 		pallet_referenda::TrackInfo {
 			name: "whitelisted_caller",
-			max_deciding: 100,
-			decision_deposit: 2 * 1000 * dollar(ACA),
+			max_deciding: 5,
+			decision_deposit: 5_000 * dollar(ACA),
 			prepare_period: 10 * MINUTES,
-			decision_period: 14 * DAYS,
+			decision_period: 10 * DAYS,
 			confirm_period: 10 * MINUTES,
 			min_enactment_period: 30 * MINUTES,
-			min_approval: Curve::make_reciprocal(1, 14, percent(96), percent(50), percent(100)),
-			min_support: Curve::make_reciprocal(1, 14 * 24, percent(1), percent(0), percent(2)),
+			min_approval: Curve::make_reciprocal(1, 10, percent(90), percent(50), percent(100)),
+			min_support: Curve::make_reciprocal(1, 10, percent(1), percent(0), percent(5)),
 		},
 	),
 	(
@@ -78,7 +78,7 @@ static ref TRACKS_DATA: [(u16, pallet_referenda::TrackInfo<Balance, BlockNumber>
 			max_deciding: 10,
 			decision_deposit: 1000 * dollar(ACA),
 			prepare_period: HOURS,
-			decision_period: 14 * DAYS,
+			decision_period: 10 * DAYS,
 			confirm_period: DAYS,
 			min_enactment_period: DAYS,
 			min_approval: Curve::make_reciprocal(4, 14, percent(80), percent(50), percent(100)),
@@ -92,25 +92,25 @@ static ref TRACKS_DATA: [(u16, pallet_referenda::TrackInfo<Balance, BlockNumber>
 			max_deciding: 20,
 			decision_deposit: 2 * 1000 * dollar(ACA),
 			prepare_period: HOURS,
-			decision_period: 14 * DAYS,
+			decision_period: 10 * DAYS,
 			confirm_period: 3 * HOURS,
 			min_enactment_period: 10 * MINUTES,
-			min_approval: Curve::make_reciprocal(1, 14, percent(96), percent(50), percent(100)),
-			min_support: Curve::make_reciprocal(1, 14, percent(1), percent(0), percent(10)),
+			min_approval: Curve::make_reciprocal(1, 10, percent(96), percent(50), percent(100)),
+			min_support: Curve::make_reciprocal(1, 10, percent(1), percent(0), percent(10)),
 		},
 	),
 	(
 		4,
 		pallet_referenda::TrackInfo {
 			name: "referendum_killer",
-			max_deciding: 100,
+			max_deciding: 20,
 			decision_deposit: 4 * 1000 * dollar(ACA),
 			prepare_period: HOURS,
-			decision_period: 14 * DAYS,
+			decision_period: 10 * DAYS,
 			confirm_period: 3 * HOURS,
 			min_enactment_period: 10 * MINUTES,
-			min_approval: Curve::make_reciprocal(1, 14, percent(96), percent(50), percent(100)),
-			min_support: Curve::make_reciprocal(1, 14, percent(1), percent(0), percent(10)),
+			min_approval: Curve::make_reciprocal(1, 10, percent(96), percent(50), percent(100)),
+			min_support: Curve::make_reciprocal(1, 10, percent(1), percent(0), percent(10)),
 		},
 	),
 ];
--- runtime/karura/src/governance/tracks.rs
+++ runtime/karura/src/governance/tracks.rs
@@ -38,37 +38,37 @@ static ref TRACKS_DATA: [(u16, pallet_referenda::TrackInfo<Balance, BlockNumber>
 			name: "root",
 			// A limit for the number of referenda on this track that can be being decided at once.
 			// For Root origin this should generally be just one.
-			max_deciding: 5,
+			max_deciding: 1,
 			// Amount that must be placed on deposit before a decision can be made.
-			decision_deposit: 20 * 1000 * dollar(KAR),
+			decision_deposit: 20_000 * dollar(KAR),
 			// Amount of time this must be submitted for before a decision can be made.
-			prepare_period: DAYS,
+			prepare_period: 2 * HOURS,
 			// Amount of time that a decision may take to be approved prior to cancellation.
-			decision_period: 14 * DAYS,
+			decision_period: 7 * DAYS,
 			// Amount of time that the approval criteria must hold before it can be approved.
 			confirm_period: DAYS,
 			// Minimum amount of time that an approved proposal must be in the dispatch queue.
 			min_enactment_period: DAYS,
 			// Minimum aye votes as percentage of overall conviction-weighted votes needed for
 			// approval as a function of time into decision period.
-			min_approval: Curve::make_reciprocal(4, 14, percent(80), percent(50), percent(100)),
+			min_approval: Curve::make_reciprocal(2, 7, percent(80), percent(50), percent(100)),
 			// Minimum pre-conviction aye-votes ("support") as percentage of overall population that
 			// is needed for approval as a function of time into decision period.
-			min_support: Curve::make_linear(14, 14, permill(5), percent(25)),
+			min_support: Curve::make_linear(7, 7, permill(1), percent(40)),
 		},
 	),
 	(
 		1,
 		pallet_referenda::TrackInfo {
 			name: "whitelisted_caller",
-			max_deciding: 100,
-			decision_deposit: 2 * 1000 * dollar(KAR),
+			max_deciding: 5,
+			decision_deposit: 5_000 * dollar(KAR),
 			prepare_period: 10 * MINUTES,
-			decision_period: 14 * DAYS,
+			decision_period: 5 * DAYS,
 			confirm_period: 10 * MINUTES,
 			min_enactment_period: 30 * MINUTES,
-			min_approval: Curve::make_reciprocal(1, 14, percent(96), percent(50), percent(100)),
-			min_support: Curve::make_reciprocal(1, 14 * 24, percent(1), percent(0), percent(2)),
+			min_approval: Curve::make_reciprocal(1, 5 * 2, percent(90), percent(50), percent(100)),
+			min_support: Curve::make_reciprocal(1, 5 * 2, percent(1), percent(0), percent(5)),
 		},
 	),
 	(
@@ -78,8 +78,8 @@ static ref TRACKS_DATA: [(u16, pallet_referenda::TrackInfo<Balance, BlockNumber>
 			max_deciding: 10,
 			decision_deposit: 1000 * dollar(KAR),
 			prepare_period: HOURS,
-			decision_period: 14 * DAYS,
-			confirm_period: DAYS,
+			decision_period: 5 * DAYS,
+			confirm_period: 6 * HOURS,
 			min_enactment_period: DAYS,
 			min_approval: Curve::make_reciprocal(4, 14, percent(80), percent(50), percent(100)),
 			min_support: Curve::make_reciprocal(7, 14, percent(10), percent(0), percent(50)),
@@ -92,25 +92,25 @@ static ref TRACKS_DATA: [(u16, pallet_referenda::TrackInfo<Balance, BlockNumber>
 			max_deciding: 20,
 			decision_deposit: 2 * 1000 * dollar(KAR),
 			prepare_period: HOURS,
-			decision_period: 14 * DAYS,
+			decision_period: 5 * DAYS,
 			confirm_period: 3 * HOURS,
 			min_enactment_period: 10 * MINUTES,
-			min_approval: Curve::make_reciprocal(1, 14, percent(96), percent(50), percent(100)),
-			min_support: Curve::make_reciprocal(1, 14, percent(1), percent(0), percent(10)),
+			min_approval: Curve::make_reciprocal(1, 5 * 2, percent(96), percent(50), percent(100)),
+			min_support: Curve::make_reciprocal(1, 5 * 2, percent(1), percent(0), percent(10)),
 		},
 	),
 	(
 		4,
 		pallet_referenda::TrackInfo {
 			name: "referendum_killer",
-			max_deciding: 100,
+			max_deciding: 20,
 			decision_deposit: 4 * 1000 * dollar(KAR),
 			prepare_period: HOURS,
-			decision_period: 14 * DAYS,
+			decision_period: 5 * DAYS,
 			confirm_period: 3 * HOURS,
 			min_enactment_period: 10 * MINUTES,
-			min_approval: Curve::make_reciprocal(1, 14, percent(96), percent(50), percent(100)),
-			min_support: Curve::make_reciprocal(1, 14, percent(1), percent(0), percent(10)),
+			min_approval: Curve::make_reciprocal(1, 5 * 2, percent(96), percent(50), percent(100)),
+			min_support: Curve::make_reciprocal(1, 5 * 2, percent(1), percent(0), percent(10)),
 		},
 	),
 ];
