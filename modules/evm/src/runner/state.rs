298a3007617f7178930e0f6a1345852097ce0f29
zjb0807 <zjb0807@qq.com>
2024-01-17 22:26:40 +0800

    remove exit_substate in create functions ref: https://github.com/rust-ethereum/evm/pull/168

--- modules/evm/src/runner/state.rs
+++ modules/evm/src/runner/state.rs
@@ -540,7 +540,6 @@ impl<'config, 'precompiles, S: StackState<'config>, P: PrecompileSet> StackExecu
 		if let Some(limit) = self.config.max_initcode_size {
 			if init_code.len() > limit {
 				self.state.metadata_mut().gasometer.fail();
-				let _ = self.exit_substate(StackExitKind::Failed);
 				return emit_exit!(ExitError::CreateContractLimit.into(), Vec::new());
 			}
 		}
@@ -581,7 +580,6 @@ impl<'config, 'precompiles, S: StackState<'config>, P: PrecompileSet> StackExecu
 		if let Some(limit) = self.config.max_initcode_size {
 			if init_code.len() > limit {
 				self.state.metadata_mut().gasometer.fail();
-				let _ = self.exit_substate(StackExitKind::Failed);
 				return emit_exit!(ExitError::CreateContractLimit.into(), Vec::new());
 			}
 		}
@@ -640,7 +638,6 @@ impl<'config, 'precompiles, S: StackState<'config>, P: PrecompileSet> StackExecu
 		if let Some(limit) = self.config.max_initcode_size {
 			if init_code.len() > limit {
 				self.state.metadata_mut().gasometer.fail();
-				let _ = self.exit_substate(StackExitKind::Failed);
 				return emit_exit!(ExitError::CreateContractLimit.into(), Vec::new());
 			}
 		}
