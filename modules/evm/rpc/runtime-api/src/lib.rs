94a04ac5ec68b9d6aba345888657971a81446dbe
zjb0807 <zjb0807@qq.com>
2024-01-29 16:21:42 +0800

    add account_call and account_create without new version

--- modules/evm/rpc/runtime-api/src/lib.rs
+++ modules/evm/rpc/runtime-api/src/lib.rs
@@ -29,39 +29,7 @@ use sp_std::vec::Vec;
 
 sp_api::decl_runtime_apis! {
 	#[api_version(2)]
-	pub trait EVMRuntimeRPCApi<Balance> where
-		Balance: Codec + MaybeDisplay + MaybeFromStr,
-	{
-		fn call(
-			from: H160,
-			to: H160,
-			data: Vec<u8>,
-			value: Balance,
-			gas_limit: u64,
-			storage_limit: u32,
-			access_list: Option<Vec<AccessListItem>>,
-			estimate: bool,
-		) -> Result<CallInfo, sp_runtime::DispatchError>;
-
-		fn create(
-			from: H160,
-			data: Vec<u8>,
-			value: Balance,
-			gas_limit: u64,
-			storage_limit: u32,
-			access_list: Option<Vec<AccessListItem>>,
-			estimate: bool,
-		) -> Result<CreateInfo, sp_runtime::DispatchError>;
-
-		fn get_estimate_resources_request(data: Vec<u8>) -> Result<EstimateResourcesRequest, sp_runtime::DispatchError>;
-
-		fn block_limits() -> BlockLimits;
-	}
-}
-
-sp_api::decl_runtime_apis! {
-	#[api_version(1)]
-	pub trait EVMRuntimeApi<Balance, AccountId> where
+	pub trait EVMRuntimeRPCApi<Balance, AccountId> where
 		Balance: Codec + MaybeDisplay + MaybeFromStr,
 		AccountId: Codec + MaybeDisplay + MaybeFromStr,
 	{
