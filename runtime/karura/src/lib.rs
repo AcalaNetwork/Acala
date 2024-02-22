89ef1e5b2167358b915877ebe490a03a775a4b0d
Xiliang Chen <xlchen1291@gmail.com>
2024-02-22 19:52:04 +1300

    2.24.0 (#2713)

--- runtime/karura/src/lib.rs
+++ runtime/karura/src/lib.rs
@@ -133,7 +133,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("karura"),
 	impl_name: create_runtime_str!("karura"),
 	authoring_version: 1,
-	spec_version: 2230,
+	spec_version: 2240,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
