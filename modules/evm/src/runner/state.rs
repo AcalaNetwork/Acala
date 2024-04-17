344699ed1056978628897f3dcd4ff0111bb86c1d
Ermal Kaleci <ermalkaleci@gmail.com>
2024-04-17 13:35:58 +0200

    trace logs and storage (#2739)

--- modules/evm/src/runner/state.rs
+++ modules/evm/src/runner/state.rs
@@ -1161,6 +1161,11 @@ impl<'config, 'precompiles, S: StackState<'config>, P: PrecompileSet> Handler
 	}
 
 	fn log(&mut self, address: H160, topics: Vec<H256>, data: Vec<u8>) -> Result<(), ExitError> {
+		event!(Log {
+			address,
+			topics: &topics,
+			data: &data,
+		});
 		self.state.log(address, topics, data);
 		Ok(())
 	}
