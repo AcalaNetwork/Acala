731b19dd1beab66e544f6dade6212904ec06bfc3
zjb0807 <zjb0807@qq.com>
2024-04-30 08:12:05 +0800

    fix tests

--- runtime/integration-tests/src/runtime.rs
+++ runtime/integration-tests/src/runtime.rs
@@ -490,7 +490,7 @@ mod mandala_only_tests {
 						bytes.len()
 					),
 					Ok(ValidTransaction {
-						priority: 51967622776640000,
+						priority: 54694050570500000,
 						requires: vec![],
 						provides: vec![],
 						longevity: 18_446_744_073_709_551_615,
