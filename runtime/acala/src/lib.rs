16f147e4cb8fc835838fcde8ee9bbf67ca23c411
Bryan Chen <xlchen1291@gmail.com>
2024-05-06 20:17:04 +1200

    bump spec version

--- runtime/acala/src/lib.rs
+++ runtime/acala/src/lib.rs
@@ -129,7 +129,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("acala"),
 	impl_name: create_runtime_str!("acala"),
 	authoring_version: 1,
-	spec_version: 2240,
+	spec_version: 2250,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
--- runtime/karura/src/lib.rs
+++ runtime/karura/src/lib.rs
@@ -133,7 +133,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("karura"),
 	impl_name: create_runtime_str!("karura"),
 	authoring_version: 1,
-	spec_version: 2240,
+	spec_version: 2250,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
