eb96173487545fe68ee67ece25c6e34c66148eed
Bryan Chen <xlchen1291@gmail.com>
2024-05-09 16:56:37 +1200

    fix srtool script

--- scripts/srtool-build.sh
+++ scripts/srtool-build.sh
@@ -5,5 +5,6 @@ set -xe
 RUSTC_VERSION=`curl -s https://raw.githubusercontent.com/paritytech/srtool/master/RUSTC_VERSION`
 PACKAGE=$PACKAGE;
 BUILD_OPTS=$BUILD_OPTS;
+PROFILE=$PROFILE
 
-docker run --rm -it -e PACKAGE=$PACKAGE -e BUILD_OPTS="$BUILD_OPTS" -v $PWD:/build -v $TMPDIR/cargo:/cargo-home paritytech/srtool:$RUSTC_VERSION $*
+docker run --rm -it -e WASM_BUILD_STD=0 -e PROFILE=$PROFILE -e PACKAGE=$PACKAGE -e BUILD_OPTS="$BUILD_OPTS" -v $PWD:/build -v $TMPDIR/cargo:/cargo-home paritytech/srtool:$RUSTC_VERSION $*
