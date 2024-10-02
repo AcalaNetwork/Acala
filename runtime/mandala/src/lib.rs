6a519a4b45b478a4e430f0eeed5fb1057136aa1e
zjb0807 <zjb0807@qq.com>
2024-10-02 09:20:40 +0800

    update orml

--- runtime/mandala/src/lib.rs
+++ runtime/mandala/src/lib.rs
@@ -2682,6 +2682,11 @@ impl_runtime_apis! {
 	}
 }
 
+cumulus_pallet_parachain_system::register_validate_block!(
+	Runtime = Runtime,
+	BlockExecutor = cumulus_pallet_aura_ext::BlockExecutor::<Runtime, Executive>,
+);
+
 #[cfg(test)]
 mod tests {
 	use super::*;
